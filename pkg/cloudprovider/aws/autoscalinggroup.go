7d58a87c0cfa4c886565bb5877f7b390e0d8a29c
Jacob Gabrielson <jacob@amazon.com>
2020-08-16 15:08:15 -0700

    Very crude initial ec2 autoscaling implementation

--- pkg/cloudprovider/aws/autoscalinggroup.go
+++ pkg/cloudprovider/aws/autoscalinggroup.go
@@ -1,5 +1,45 @@
 package aws
 
+import (
+	//"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	//	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
+)
+
 // AutoScalingGroup implements the NodeGroup CloudProvider for AWS EC2 AutoScalingGroups
 type AutoScalingGroup struct {
+	name   string
+	client autoscalingiface.AutoScalingAPI
+}
+
+func NewDefaultAutoScalingGroup(name string) (asg *AutoScalingGroup, err error) {
+	sess := session.Must(session.NewSession())
+	svc := autoscaling.New(sess)
+	return NewAutoScalingGroup(svc, name), nil
+}
+
+func NewAutoScalingGroup(svc autoscalingiface.AutoScalingAPI, name string) *AutoScalingGroup {
+	return &AutoScalingGroup{name: name,
+		client: svc,
+	}
+}
+
+// Name returns the name of the node group
+func (asg *AutoScalingGroup) Name() string {
+	return asg.name
+}
+
+// SetReplicas sets the NodeGroups's replica count
+func (asg *AutoScalingGroup) SetReplicas(value int) error {
+	newSize := aws.Int64(int64(value))
+	_, err := asg.client.UpdateAutoScalingGroup(&autoscaling.UpdateAutoScalingGroupInput{
+		MaxSize:         newSize,
+		MinSize:         newSize,
+		DesiredCapacity: newSize,
+	})
+	return err
 }
---
+++ pkg/cloudprovider/aws/autoscalinggroup_test.go
@@ -0,0 +1,34 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
+)
+
+type mockedUpdateAutoScalingGroup struct {
+	autoscalingiface.AutoScalingAPI
+	Resp  autoscaling.UpdateAutoScalingGroupOutput
+	Error error
+}
+
+func (m mockedUpdateAutoScalingGroup) UpdateAutoScalingGroup(*autoscaling.UpdateAutoScalingGroupInput) (*autoscaling.UpdateAutoScalingGroupOutput, error) {
+	if m.Error != nil {
+		return nil, m.Error
+	} else {
+		return &m.Resp, nil
+	}
+}
+
+func TestUpdateAutoScalingGroupSuccess(t *testing.T) {
+	client := mockedUpdateAutoScalingGroup{
+		Resp:  autoscaling.UpdateAutoScalingGroupOutput{},
+		Error: nil,
+	}
+	asg := NewAutoScalingGroup(client, "spatula")
+	err := asg.SetReplicas(23)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
