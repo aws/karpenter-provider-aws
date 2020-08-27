c719d462645b1b4d5fe27d11512715a86748a256
Ellis Tarn <ellistarn@gmail.com>
2020-08-27 16:30:57 -0700

    Purged all licenses

--- hack/boilerplate.sh
+++ hack/boilerplate.sh
@@ -2,7 +2,7 @@
 
 for i in $(find ./pkg -name *.go)  # or whatever other pattern...
 do
-  if ! grep -q Copyright $i
+  if ! grep -q "Apache License" $i
   then
     cat hack/boilerplate.go.txt $i >$i.new && mv $i.new $i
   fi
