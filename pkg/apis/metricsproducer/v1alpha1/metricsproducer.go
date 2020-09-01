6af9dc0028ffdc173fa5dc80c0273cf2c143abf8
Ellis Tarn <ellistarn@gmail.com>
2020-09-01 11:31:38 -0700

    Change QueueProvider from 'provider' to 'type' for parity with ScalableNodeGroup

--- pkg/apis/metricsproducer/v1alpha1/metricsproducer.go
+++ pkg/apis/metricsproducer/v1alpha1/metricsproducer.go
@@ -1,3 +1,16 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 // Package v1alpha1 holds definitions for MetricsProducer
 // +kubebuilder:object:generate=true
 // +groupName=karpenter.sh
@@ -42,16 +55,16 @@ type PendingPodsSpec struct {
 
 // QueueSpec outputs metrics for a queue.
 type QueueSpec struct {
-	QueueProvider QueueProvider `json:"provider"`
-	ID            string        `json:"id"`
+	Type QueueProviderType `json:"type"`
+	ID   string            `json:"id"`
 }
 
-// QueueProvider corresponds to an implementation of a queue
-type QueueProvider string
+// QueueProviderType corresponds to an implementation of a queue
+type QueueProviderType string
 
 // QueueProvider enum
 const (
-	AWSSQSQueueProvider QueueProvider = "AWSSQSQueueProvider"
+	AWSSQSQueueProvider QueueProviderType = "AWSSQSQueueProvider"
 )
 
 // MetricsProducer is the Schema for the MetricsProducers API
