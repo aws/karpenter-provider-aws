0836fff872b6fd9dc81adc92c94ddfeb43bf9db0
Jacob Gabrielson <jacob@amazon.com>
2020-09-22 18:01:01 -0700

    WIP extract ng name and cluster name from ARN

--- pkg/cloudprovider/nodegroup/aws/managednodegroup.go
+++ pkg/cloudprovider/nodegroup/aws/managednodegroup.go
@@ -16,16 +16,14 @@ package aws
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/eks"
 	"github.com/aws/aws-sdk-go/service/eks/eksiface"
 	"github.com/ellistarn/karpenter/pkg/cloudprovider"
+	"github.com/pkg/errors"
+	"strings"
 )
-
-// ManagedNodeGroupProvider TODO(jacob@)
-type ManagedNodeGroupProvider struct {
-	ClusterName string
-}
 
 // NewNodeGroup TODO(jacob@)
 func (m *ManagedNodeGroupProvider) NewNodeGroup(name string) cloudprovider.NodeGroup {
@@ -34,25 +32,50 @@ func (m *ManagedNodeGroupProvider) NewNodeGroup(name string) cloudprovider.NodeG
 
 // ManagedNodeGroup implements the NodeGroup CloudProvider for AWS EKS Managed Node Groups
 type ManagedNodeGroup struct {
-	Client      eksiface.EKSAPI
-	GroupName   string
-	ClusterName string
+	GroupName string
+	Client    eksiface.EKSAPI
 }
 
 // NewDefaultManagedNodeGroup TODO(jacob@)
-func NewDefaultManagedNodeGroup(name string, clusterName string) *ManagedNodeGroup {
+func NewDefaultManagedNodeGroup(name string) *ManagedNodeGroup {
 	return &ManagedNodeGroup{
-		Client:      eks.New(session.Must(session.NewSession())),
-		GroupName:   name,
-		ClusterName: clusterName,
+		Client:    eks.New(session.Must(session.NewSession())),
+		GroupName: name,
 	}
+}
+
+type clusterId struct {
+	clusterName   string
+	nodegroupName string
+}
+
+func parseClusterId(fromArn string) (*clusterId, error) {
+	nodegroupArn, err := arn.Parse(fromArn)
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to parse GroupName %s as ARN", fromArn)
+	}
+	// Example ARN:
+	// arn:aws:eks:us-west-2:741206201142:nodegroup/ridiculous-sculpture-1594766004/ng-0b663e8a/aeb9a7fe-69d6-21f0-cb41-fb9b03d3aaa9
+	//                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^
+	//                                              |                               |
+	//                                              cluster-name                    nodegroup-name
+	components := strings.Split(nodegroupArn.Resource, "/")
+	if len(components) != 4 {
+		return nil, errors.Errorf("ARN resource format does not contain expected 4 components: %s", nodegroupArn.Resource)
+	}
+	return &clusterId{
+		clusterName:   components[1],
+		nodegroupName: components[2],
+	}, nil
 }
 
 // SetReplicas TODO(jacob@)
 func (mng *ManagedNodeGroup) SetReplicas(value int) error {
+	id, err := parseClusterId(mng.GroupName)
+
 	_, err := mng.Client.UpdateNodegroupConfig(&eks.UpdateNodegroupConfigInput{
-		ClusterName:   aws.String(mng.ClusterName),
-		NodegroupName: aws.String(mng.GroupName),
+		ClusterName:   &id.clusterName,
+		NodegroupName: &id.nodegroupName,
 		ScalingConfig: &eks.NodegroupScalingConfig{
 			DesiredSize: aws.Int64(int64(value)),
 		},
