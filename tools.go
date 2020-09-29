ccc5d6a4a8fde33874eb66987aa5c69eba7eeb7f
Ellis Tarn <ellistarn@gmail.com>
2020-09-29 12:51:22 -0700

    Added ginkgo to tools

--- tools.go
+++ tools.go
@@ -5,6 +5,7 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/google/ko/cmd/ko"
+	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kubebuilder/cmd"
 )
