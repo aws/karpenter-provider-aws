6c25eb0c2974a8674b126017a7c550b4b6ac78fb
Prateek Gogia <pgogia@amazon.com>
2020-11-30 17:08:42 -0600

    Fixed pointers comparison

--- pkg/controllers/scalablenodegroup/v1alpha1/suite_test.go
+++ pkg/controllers/scalablenodegroup/v1alpha1/suite_test.go
@@ -54,7 +54,7 @@ var _ = AfterSuite(func() {
 
 var _ = Describe("Examples", func() {
 	var ns *environment.Namespace
-	var defaultReplicaCount = ptr.Int32(3)
+	var defaultReplicaCount = int32(3)
 
 	var sng *v1alpha1.ScalableNodeGroup
 
@@ -66,7 +66,7 @@ var _ = Describe("Examples", func() {
 		fakeCloudProvider.NodeGroupStable = true
 		fakeCloudProvider.NodeGroupMessage = ""
 		fakeCloudProvider.WantErr = nil
-		sng.Spec.Replicas = defaultReplicaCount
+		sng.Spec.Replicas = &defaultReplicaCount
 		fakeCloudProvider.NodeReplicas[sng.Spec.ID] = ptr.Int32(0)
 	})
 
@@ -83,24 +83,24 @@ var _ = Describe("Examples", func() {
 	Context("ScalableNodeGroup Reconcile tests", func() {
 		It("Test reconciler to scale up nodes", func() {
 			Expect(fakeController.Reconcile(sng)).To(Succeed())
-			Expect(fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
+			Expect(*fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
 		})
 
 		It("Test reconciler to scale down nodes", func() {
 			fakeCloudProvider.NodeReplicas[sng.Spec.ID] = ptr.Int32(10) // set existing replicas higher than desired
 			Expect(fakeController.Reconcile(sng)).To(Succeed())
-			Expect(fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
+			Expect(*fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
 		})
 
 		It("Test reconciler to make no change to node count", func() {
-			fakeCloudProvider.NodeReplicas[sng.Spec.ID] = defaultReplicaCount // set existing replicas equal to desired
+			fakeCloudProvider.NodeReplicas[sng.Spec.ID] = &defaultReplicaCount // set existing replicas equal to desired
 			Expect(fakeController.Reconcile(sng)).To(Succeed())
-			Expect(fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
+			Expect(*fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
 		})
 
 		It("Scale up nodes when not node group is stabilized and check status condition", func() {
 			Expect(fakeController.Reconcile(sng)).To(Succeed())
-			Expect(fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
+			Expect(*fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
 			Expect(sng.StatusConditions().GetCondition(v1alpha1.Stabilized).IsTrue()).To(Equal(true))
 			Expect(sng.StatusConditions().GetCondition(v1alpha1.Stabilized).Message).To(Equal(""))
 		})
@@ -110,7 +110,7 @@ var _ = Describe("Examples", func() {
 			fakeCloudProvider.NodeGroupStable = false
 			fakeCloudProvider.NodeGroupMessage = dummyMessage
 			Expect(fakeController.Reconcile(sng)).To(Succeed())
-			Expect(fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
+			Expect(*fakeCloudProvider.NodeReplicas[sng.Spec.ID]).To(Equal(defaultReplicaCount))
 			Expect(sng.StatusConditions().GetCondition(v1alpha1.Stabilized).IsFalse()).To(Equal(true))
 			Expect(sng.StatusConditions().GetCondition(v1alpha1.Stabilized).Message).To(Equal(dummyMessage))
 		})
