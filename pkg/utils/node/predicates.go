efa39114054cb9066acaca93def3dbebcbdc1c5d
njtran <nichotr@amazon.com>
2021-03-04 11:19:38 -0800

    removed generalized underutilization predicate

--- pkg/utils/node/predicates.go
+++ pkg/utils/node/predicates.go
@@ -17,17 +17,8 @@ package node
 import (
 	"github.com/awslabs/karpenter/pkg/apis/provisioning/v1alpha1"
 	"github.com/awslabs/karpenter/pkg/utils/scheduling"
-	"go.uber.org/zap"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	"math"
 	"time"
-)
-
-const (
-	// TODO: Decide if this can be configured in the spec, and implement.
-	// Only consider a node underutilizd if no non-daemon pods are scheduled to it
-	UnderutilizedPercentage = 0
 )
 
 func IsReadyAndSchedulable(node v1.Node) bool {
@@ -51,40 +42,12 @@ func IsPastTTL(node *v1.Node) bool {
 	return time.Now().After(ttlTime)
 }
 
-// IsUnderutilized returns true if all of a node's pod's resource requests sum to less than the given thresholds
+// IsUnderutilized returns if the node has 0 non-daemosnset pods
 func IsUnderutilized(node *v1.Node, pods []*v1.Pod) bool {
-	threshold := v1.ResourceList{
-		v1.ResourceCPU: *resource.NewMilliQuantity(
-			roundQuantity(node.Status.Capacity.Cpu().MilliValue(), UnderutilizedPercentage),
-			resource.DecimalSI),
-		v1.ResourceMemory: *resource.NewQuantity(
-			roundQuantity(node.Status.Capacity.Memory().Value(), UnderutilizedPercentage),
-			resource.DecimalSI),
-		v1.ResourcePods: *resource.NewQuantity(
-			roundQuantity(node.Status.Capacity.Pods().Value(), UnderutilizedPercentage),
-			resource.BinarySI),
-	}
-	cpuTotal := &resource.Quantity{}
-	memoryTotal := &resource.Quantity{}
-	podTotal := &resource.Quantity{}
-
 	for _, pod := range pods {
 		if !scheduling.IsOwnedByDaemonSet(pod) {
-			zap.S().Debugf("This is the pod name %s", pod.Name)
-			resources := scheduling.GetResources(&pod.Spec)
-			cpuTotal.Add(*resources.Cpu())
-			memoryTotal.Add(*resources.Memory())
-			podTotal.Add(*resource.NewQuantity(1, resource.BinarySI))
+			return false
 		}
 	}
-
-	return (cpuTotal.Cmp(*threshold.Cpu()) == -1 &&
-		memoryTotal.Cmp(*threshold.Memory()) == -1 &&
-		podTotal.Cmp(*threshold.Pods()) == -1) ||
-		(cpuTotal.IsZero() && memoryTotal.IsZero() && podTotal.IsZero())
-}
-
-// roundQuantity calculates the rounded int value of a percentage of val
-func roundQuantity(val int64, percentage float64) int64 {
-	return int64(math.Round(float64(val) * percentage))
+	return true
 }
