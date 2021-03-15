a32419f7d1fc553016132e4af75098594e44baf1
Ellis Tarn <ellistarn@gmail.com>
2021-03-15 14:45:34 -0700

    Included yq in tools and upgraded to v4

--- tools/tools.go
+++ tools/tools.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/fzipp/gocyclo"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/google/ko/cmd/ko"
+	_ "github.com/mikefarah/yq/v4"
 	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kubebuilder/cmd"
