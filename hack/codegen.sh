062a229d4cf3bf6b8283def9f000f708e048249e
Ellis Tarn <ellistarn@gmail.com>
2021-03-15 15:15:11 -0700

    Included yq in tools and upgraded to v4 (#290)

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -14,16 +14,11 @@ mv config/provisioning.karpenter.sh_provisioners.yaml config/provisioner-crd.yam
 mv config/manifests.yaml config/webhooks.yaml
 
 # Fixup Webhook code generation. controller-gen assumes using kustomize; we do this instead
-yq w -i -d0 ./config/webhooks.yaml 'metadata.name' karpenter-mutating-webhook-configuration
-yq w -i -d0 ./config/webhooks.yaml 'metadata.annotations[cert-manager.io/inject-ca-from]' karpenter/karpenter-serving-cert
-yq w -i -d0 ./config/webhooks.yaml 'webhooks[0].clientConfig.service.name' karpenter-webhook-service
-yq w -i -d0 ./config/webhooks.yaml 'webhooks[0].clientConfig.service.namespace' karpenter
-yq d -i -d0 ./config/webhooks.yaml 'webhooks[0].admissionReviewVersions[0]'
-yq w -i -d1 ./config/webhooks.yaml 'metadata.name' karpenter-validating-webhook-configuration
-yq w -i -d1 ./config/webhooks.yaml 'metadata.annotations[cert-manager.io/inject-ca-from]' karpenter/karpenter-serving-cert
-yq w -i -d1 ./config/webhooks.yaml 'webhooks[0].clientConfig.service.name' karpenter-webhook-service
-yq w -i -d1 ./config/webhooks.yaml 'webhooks[0].clientConfig.service.namespace' karpenter
-yq d -i -d1 ./config/webhooks.yaml 'webhooks[0].admissionReviewVersions[0]'
+yq e -i '.metadata.name = "karpenter-" + .metadata.name' ./config/webhooks.yaml
+yq e -i '.metadata.annotations = { "cert-manager.io/inject-ca-from" : "karpenter/karpenter-serving-cert" }' ./config/webhooks.yaml
+yq e -i '.webhooks[].clientConfig.service.name = "karpenter-webhook-service"' ./config/webhooks.yaml
+yq e -i '.webhooks[].clientConfig.service.namespace = "karpenter"' ./config/webhooks.yaml
+yq e -i 'del(.webhooks[].admissionReviewVersions[0])' ./config/webhooks.yaml
 
 # Hack to remove v1.AdmissionReview until https://github.com/kubernetes-sigs/controller-runtime/issues/1161 is fixed
 perl -pi -e 's/^  - v1$$//g' config/webhooks.yaml
