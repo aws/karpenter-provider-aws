6134bfc901afa82ace71cf1c893d5c463203ad36
jacob <jacob@amazon.com>
2021-03-25 16:45:16 -0700

    check

--- pkg/utils/functional/suite_test.go
+++ pkg/utils/functional/suite_test.go
@@ -13,43 +13,43 @@ func TestFunctional(t *testing.T) {
 }
 
 var _ = Describe("Functional", func() {
-	var (
-		emptyInt32 []int32
-		//		oneInt32   []int32
-		//longInt32  []int32
-	)
+	// var (
+	// 	emptyInt32 []int32
+	// 	//		oneInt32   []int32
+	// 	//longInt32  []int32
+	// )
 
-	Context("With empty slice", func() {
-		Specify("should return empty slice", func() {
-			//Expect(functional.GreaterThanInt32(emptyInt32, 23)).To(BeEmpty())
-			Expect(GreaterThanInt32(emptyInt32, 23)).To(BeEmpty())
-			Expect(LessThanInt32(emptyInt32, 23)).To(BeEmpty())
-			alwaysTrue := func(_, _ int32) bool { return true }
-			Expect(FilterInt32(emptyInt32, 23, alwaysTrue)).To(BeEmpty())
-		})
-	})
+	// Context("With empty slice", func() {
+	// 	Specify("should return empty slice", func() {
+	// 		//Expect(functional.GreaterThanInt32(emptyInt32, 23)).To(BeEmpty())
+	// 		Expect(GreaterThanInt32(emptyInt32, 23)).To(BeEmpty())
+	// 		Expect(LessThanInt32(emptyInt32, 23)).To(BeEmpty())
+	// 		alwaysTrue := func(_, _ int32) bool { return true }
+	// 		Expect(FilterInt32(emptyInt32, 23, alwaysTrue)).To(BeEmpty())
+	// 	})
+	// })
 
-	Context("MergeInto", func() {
-		It("does nothing with empty", func() {
-			dest := struct {
-				a int
-				b int
-			}{a: 2, b: 3}
-			orig := dest
-			MergeInto(dest)
-			Expect(dest).To(Equal(orig))
-		})
+	// Context("MergeInto", func() {
+	// 	It("does nothing with empty", func() {
+	// 		dest := struct {
+	// 			a int
+	// 			b int
+	// 		}{a: 2, b: 3}
+	// 		orig := dest
+	// 		MergeInto(dest)
+	// 		Expect(dest).To(Equal(orig))
+	// 	})
 
-		It("does nothing with copy", func() {
-			dest := struct {
-				a int
-				b int
-			}{a: 2, b: 3}
-			orig := dest
-			same := dest
-			MergeInto(&dest, &same)
-			Expect(dest).To(Equal(orig))
-		})
+	// 	It("does nothing with copy", func() {
+	// 		dest := struct {
+	// 			a int
+	// 			b int
+	// 		}{a: 2, b: 3}
+	// 		orig := dest
+	// 		same := dest
+	// 		MergeInto(&dest, &same)
+	// 		Expect(dest).To(Equal(orig))
+	// 	})
 
-	})
+	// })
 })
