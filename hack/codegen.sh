5d9ed0ca3ca9c7685ac76d24e78679f85be06156
Clement Rey <clementrey.dev@gmail.com>
2021-05-06 16:40:23 +0200

    fix: moving all files related to helm in ./charts/karpenter

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -5,23 +5,23 @@ controller-gen \
     webhook \
     crd:trivialVersions=false \
     paths="./pkg/..." \
-    output:crd:artifacts:config=config/templates \
-    output:webhook:artifacts:config=config/templates
+    output:crd:artifacts:config=charts/karpenter/templates \
+    output:webhook:artifacts:config=charts/karpenter/templates
 
 ./hack/boilerplate.sh
 
-mv config/templates/provisioning.karpenter.sh_provisioners.yaml config/templates/provisioner-crd.yaml
-mv config/templates/manifests.yaml config/templates/webhooks.yaml
+mv charts/karpenter/templates/provisioning.karpenter.sh_provisioners.yaml charts/karpenter/templates/provisioner-crd.yaml
+mv charts/karpenter/templates/manifests.yaml charts/karpenter/templates/webhooks.yaml
 
 # Fixup Webhook code generation. controller-gen assumes using kustomize; we do this instead
-yq e -i '.metadata.name = "karpenter-" + .metadata.name' ./config/templates/webhooks.yaml
-yq e -i '.metadata.annotations = { "cert-manager.io/inject-ca-from" : "karpenter/karpenter-serving-cert" }' ./config/templates/webhooks.yaml
-yq e -i '.webhooks[].clientConfig.service.name = "karpenter-webhook-service"' ./config/templates/webhooks.yaml
-yq e -i '.webhooks[].clientConfig.service.namespace = "karpenter"' ./config/templates/webhooks.yaml
-yq e -i 'del(.webhooks[].admissionReviewVersions[0])' ./config/templates/webhooks.yaml
+yq e -i '.metadata.name = "karpenter-" + .metadata.name' charts/karpenter/templates/webhooks.yaml
+yq e -i '.metadata.annotations = { "cert-manager.io/inject-ca-from" : "karpenter/karpenter-serving-cert" }' charts/karpenter/templates/webhooks.yaml
+yq e -i '.webhooks[].clientConfig.service.name = "karpenter-webhook-service"' charts/karpenter/templates/webhooks.yaml
+yq e -i '.webhooks[].clientConfig.service.namespace = "karpenter"' charts/karpenter/templates/webhooks.yaml
+yq e -i 'del(.webhooks[].admissionReviewVersions[0])' charts/karpenter/templates/webhooks.yaml
 
 # Hack to remove v1.AdmissionReview until https://github.com/kubernetes-sigs/controller-runtime/issues/1161 is fixed
-perl -pi -e 's/^  - v1$$//g' config/templates/webhooks.yaml
+perl -pi -e 's/^  - v1$$//g' charts/karpenter/templates/webhooks.yaml
 
 # CRDs don't currently jive with VolatileTime, which has an Any type.
-perl -pi -e 's/Any/string/g' config/templates/provisioner-crd.yaml
+perl -pi -e 's/Any/string/g' charts/karpenter/templates/provisioner-crd.yaml
