ff3c045f7def7445989254400a353f0ec6daedf5
Ellis Tarn <ellistarn@gmail.com>
2021-06-30 14:07:21 -0700

    Moved from github.com/google/ko/cmd/ko -> github.com/google/ko

--- tools/tools.go
+++ tools/tools.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/ahmetb/gen-crd-api-reference-docs"
 	_ "github.com/fzipp/gocyclo"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/google/ko/cmd/ko"
+	_ "github.com/google/ko"
 	_ "github.com/mikefarah/yq/v4"
 	_ "github.com/mitchellh/golicense"
 	_ "github.com/onsi/ginkgo/ginkgo"
