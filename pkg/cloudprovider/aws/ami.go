9f24c5742280adbc977c0af6668cc1bdb0c97267
jacob <jacob@amazon.com>
2021-09-10 11:32:49 -0700

    pr fback

--- pkg/cloudprovider/aws/ami.go
+++ pkg/cloudprovider/aws/ami.go
@@ -53,8 +53,7 @@ func (p *AMIProvider) Get(ctx context.Context, constraints *Constraints) (string
 	if *constraints.Architecture == v1alpha3.ArchitectureArm64 {
 		architectureSuffix = "-arm64"
 	}
-	// TODO: support for the "amazon-linux-2-gpu" AMI for nvidia use
-	// cases
+	// TODO: support for the "amazon-linux-2-gpu" AMI for nvidia use cases
 	name := fmt.Sprintf("/aws/service/eks/optimized-ami/%s/amazon-linux-2%s/recommended/image_id", version, architectureSuffix)
 	if id, ok := p.cache.Get(name); ok {
 		return id.(string), nil
