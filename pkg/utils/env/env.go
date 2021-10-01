7541834adfa659b308f881614e4453876feb866a
Brandon Wagner <wagnerbm@amazon.com>
2021-10-01 14:34:26 -0500

    add kube client qps configuration w/ env vars and cli overrides (#723)

---
+++ pkg/utils/env/env.go
@@ -0,0 +1,61 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// WithDefaultString returns the string value of the supplied environ variable or, if not
+// present, the supplied default value
+func WithDefaultString(key string, def string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	return val
+}
+
+// WithDefaultInt returns the int value of the supplied environ variable or, if not present,
+// the supplied default value. If the int conversion fails, returns the default
+func WithDefaultInt(key string, def int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		fmt.Printf("COULDN'T FIND KEY %s", key)
+		return def
+	}
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		fmt.Printf("CONVERSION FAILED FOR %s w/ value %s", key, val)
+		return def
+	}
+	return i
+}
+
+// WithDefaultBool returns the boolvalue of the supplied environ variable or, if not present,
+// the supplied default value. If the conversion fails, returns the default
+func WithDefaultBool(key string, def bool) bool {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	b, err := strconv.ParseBool(val)
+	if err != nil {
+		return def
+	}
+	return b
+}
