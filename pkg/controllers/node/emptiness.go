e5f6cc97fd48ae9f2c63e840faf3c0072c19700e
njtran <nichotr@amazon.com>
2021-12-08 17:54:14 -0800

    Added succeded pods to emptiness criteria

--- pkg/controllers/node/emptiness.go
+++ pkg/controllers/node/emptiness.go
@@ -88,7 +88,7 @@ func (r *Emptiness) isEmpty(ctx context.Context, n *v1.Node) (bool, error) {
 	}
 	for i := range pods.Items {
 		p := pods.Items[i]
-		if pod.HasFailed(&p) {
+		if pod.HasFinished(&p) {
 			continue
 		}
 		if !pod.IsOwnedByDaemonSet(&p) && !pod.IsOwnedByNode(&p) {
