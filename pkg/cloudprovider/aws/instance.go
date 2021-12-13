acbf53f403db09397e788760953f2723f66d6d79
mslavin-tibco <64420549+mslavin-tibco@users.noreply.github.com>
2021-12-13 15:45:34 -0600

    Allow EC2 "Name" tag to be supplied in the provisioner spec (#964)
    * Allow EC2 Name tag to be supplied in provisioner spec
    
    * Updated docs to mention default value of Name tag if not supplied
    
    * Update website/content/en/docs/AWS/provisioning.md
    
    Fixing typo in docs
    
    Co-authored-by: Ellis Tarn <ellistarn@gmail.com>
    
    * Condensed some logic based on PR feedback
    
    * Default the EC2 Name tag to karpenter.sh/cluster/<cluster-name>/provisioner/<provisioner-name>
    
    * Simplified validation and allow any of the managed tags to be overridden
    
    * Removed unused ctx from validateTags parameters
    
    Co-authored-by: Ellis Tarn <ellistarn@gmail.com>

--- pkg/cloudprovider/aws/instance.go
+++ pkg/cloudprovider/aws/instance.go
@@ -33,7 +33,6 @@ import (
 	"github.com/aws/karpenter/pkg/apis/provisioning/v1alpha5"
 	"github.com/aws/karpenter/pkg/cloudprovider"
 	"github.com/aws/karpenter/pkg/cloudprovider/aws/apis/v1alpha1"
-	"github.com/aws/karpenter/pkg/utils/injection"
 )
 
 type InstanceProvider struct {
@@ -124,7 +123,7 @@ func (p *InstanceProvider) launchInstances(ctx context.Context, constraints *v1a
 		TagSpecifications: []*ec2.TagSpecification{
 			{
 				ResourceType: aws.String(ec2.ResourceTypeInstance),
-				Tags:         v1alpha1.MergeTags(v1alpha1.ManagedTagsFor(injection.GetOptions(ctx).ClusterName), constraints.Tags),
+				Tags:         v1alpha1.MergeTags(ctx, constraints.Tags),
 			},
 		},
 		// OnDemandOptions are allowed to be specified even when requesting spot
