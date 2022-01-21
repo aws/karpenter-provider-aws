278326c659d8b3992a4b7443f9e36269bc19e5bc
Alex Kestner <alex.kestner@gmail.com>
2022-01-20 20:35:15 -0700

    refactors javascript docs version warning and sets up JS builds

---
+++ website/assets/js/index.js
@@ -0,0 +1,4 @@
+import { versionWarning } from "./versionWarning";
+
+// show warning when viewing outdated or preview docs
+document.addEventListener('DOMContentLoaded', versionWarning, false);
---
+++ website/assets/js/versionWarning/index.js
@@ -0,0 +1,21 @@
+export function versionWarning() {
+  const viewingVersion = window.location.pathname.split('/')[1]
+  if (viewingVersion) {
+    const latestVersion = [
+      ...document.querySelectorAll("#navbarVersionSelector .dropdown-item")
+    ].map(el => el.dataset.docsVersion)[0]
+
+    if (viewingVersion === "preview" || viewingVersion !== latestVersion) {
+      const alertElement = document.createElement('div')
+      alertElement.classList.add('alert', 'alert-warning')
+      alertElement.innerHTML = `
+        <h3>You are viewing Karpenter's <strong>${viewingVersion}</strong> documentation</h3>
+        <p>
+          Karpenter <strong>${viewingVersion}</strong> is not the latest stable release. 
+          For up-to-date documentation, see the <a href="/${latestVersion}">latest version</a>.
+        </p>
+      `
+      document.querySelector('.td-content').prepend(alertElement)
+    }
+  }
+}
