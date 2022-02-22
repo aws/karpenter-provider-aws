3eca04b81094f73e256a59c1f680c1266843762f
Ryan Maleki <ryan@maleki.email>
2022-02-21 16:06:28 -0800

    Add empty test suite to get rid of the ginggo warning

---
+++ pkg/controllers/provisioning/binpacking/suite_test.go
@@ -0,0 +1,13 @@
+package binpacking_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestBinpacking(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Binpacking Suite")
+}
