997e371b21befe6f1d4a86c7efbcfb5f861874c7
Ryan Maleki <ryan@maleki.email>
2022-02-24 13:17:50 -0800

    Turn steps into a loop, end execution if one step fails and add end of line to files

--- website/content/en/preview/getting-started/scripts/run-all-steps.sh
+++ website/content/en/preview/getting-started/scripts/run-all-steps.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-echo "Step 0"
-source step00-karpenter-version.sh
-echo "Step 1"
-source step01-config.sh
-echo "Step 2"
-source step02-create-cluster.sh
-echo "Step 3"
-source step03-iam-cloud-formation.sh
-echo "Step 4"
-source step04-grant-access.sh
-echo "Step 5"
-source step05-controller-iam.sh
-echo "Step 6"
-source step06-install-helm-chart.sh
-echo "Step 7"
-source step07-apply-helm-charts.sh
+
+set -euo pipefail #fail if one step fails
+
+declare -a steps=(
+  step00-karpenter-version.sh
+  step01-config.sh
+  step02-create-cluster.sh
+  step03-iam-cloud-formation.sh
+  step04-grant-access.sh
+  step05-controller-iam.sh
+  step06-install-helm-chart.sh
+  step07-apply-helm-chart.sh
+)
+
+i=0
+for step in "${steps[@]}"
+do
+  echo "Step $i"
+   source $step
+   (( i += 1 ))
+done
