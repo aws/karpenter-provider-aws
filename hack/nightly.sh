f1bd123270db180718da631170a9b027f9c6cfc3
Ryan Maleki <ryan@maleki.email>
2022-03-29 14:25:15 -0700

    Add CLOUD_PROVIDER back

--- hack/nightly.sh
+++ hack/nightly.sh
@@ -4,6 +4,11 @@ RELEASE_REPO=${RELEASE_REPO:-public.ecr.aws/z3c6l6z9/karpenter-nightly}
 RELEASE_VERSION=${RELEASE_VERSION:-$(date "${NIGHTLY_TAG_FMT}")}
 RELEASE_PLATFORM="--platform=linux/amd64,linux/arm64"
 
+if [ -z "$CLOUD_PROVIDER" ]; then
+    echo "CLOUD_PROVIDER environment variable is not set: 'export CLOUD_PROVIDER=aws'"
+    exit 1
+fi
+
 image() {
     aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${RELEASE_REPO}
     CONTROLLER_DIGEST=$(GOFLAGS=${GOFLAGS} KO_DOCKER_REPO=${RELEASE_REPO} ko publish -B -t ${RELEASE_VERSION} ${RELEASE_PLATFORM} ./cmd/controller)
