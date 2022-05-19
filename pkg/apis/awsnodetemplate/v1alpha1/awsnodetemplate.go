7280acc02372e7fcb982f96fe4bf514182e6ed94
Suket Sharma <sukets@amazon.com>
2022-05-19 08:49:03 -0700

    Adds AWSNodeTemplate CRD for AWS specific config (#1822)
    * Adds AWSProvider CRD for AWS specific config
    
    * s/awsprovider/awsnodeconfig
    
    * s/awsnodeconfig/awsnodetemplate
    
    * Fixing some typos from all the renaming

---
+++ pkg/apis/awsnodetemplate/v1alpha1/awsnodetemplate.go
@@ -0,0 +1,39 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// AWSNodeTemplateSpec is the top level specification for the AWS Karpenter Provider.
+// This will contain configuration necessary to launch instances in AWS.
+type AWSNodeTemplateSpec struct {
+	// UserData to be applied to the provisioned nodes.
+	// It must be in the appropriate format based on the AMIFamily in use. Karpenter will merge certain fields into
+	// this UserData to ensure nodes are being provisioned with the correct configuration.
+	// +optional
+	UserData *string `json:"userData,omitempty"`
+}
+
+// AWSNodeTemplate is the Schema for the AWSNodeTemplate API
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:path=awsnodetemplate,scope=Cluster,categories=karpenter
+type AWSNodeTemplate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec AWSNodeTemplateSpec `json:"spec,omitempty"`
+}
