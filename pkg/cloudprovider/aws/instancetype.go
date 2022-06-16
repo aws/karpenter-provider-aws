edf8c7cc25b108f17835134c5f8cb31fde46dc54
Brandon <wagnerbm@amazon.com>
2022-06-16 11:24:47 -0500

    feat: add instance-hypervisor as an AWS node label

--- pkg/cloudprovider/aws/instancetype.go
+++ pkg/cloudprovider/aws/instancetype.go
@@ -128,6 +128,7 @@ func (i *InstanceType) computeRequirements() scheduling.Requirements {
 		v1alpha1.LabelInstanceGPUManufacturer: sets.NewSet(),
 		v1alpha1.LabelInstanceGPUCount:        sets.NewSet(),
 		v1alpha1.LabelInstanceGPUMemory:       sets.NewSet(),
+		v1alpha1.LabelInstanceHypervisor:      sets.NewSet(aws.StringValue(i.Hypervisor)),
 	}
 	// Instance Type Labels
 	instanceTypeParts := strings.Split(aws.StringValue(i.InstanceType), ".")
