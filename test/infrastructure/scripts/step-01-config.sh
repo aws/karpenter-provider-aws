e0ea58651b6ccfc0897eadb218a45ece00ccf0d1
Brandon <wagnerbm@amazon.com>
2022-07-22 15:16:46 -0500

    Add Karpenter flux sync resources for tekton tests

--- test/infrastructure/scripts/step-01-config.sh
+++
@@ -1,5 +0,0 @@
-export CLUSTER_NAME="${CLUSTER_NAME:-karpenter-test-infrastructure}"
-export AWS_PROFILE="${AWS_PROFILE:-karpenter-ci}"
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-export AWS_REGION="${AWS_REGION:-us-west-2}"
-export AWS_PAGER=""
