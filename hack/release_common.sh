7c7f339c963ad290e2f137075ec59a6a2368512a
Ryan Maleki <spring1843@users.noreply.github.com>
2022-07-28 11:06:36 -0700

    Revert "Fix logging into public ECR (#2204)" (#2211)
    This reverts commit 8b24a5074a7e34575d1950f8c4c07a12c4e608fa.

--- hack/release_common.sh
+++ hack/release_common.sh
@@ -24,7 +24,7 @@ requireCloudProvider(){
 }
 
 authenticate() {
-  aws ecr-public get-login-password --region us-east-1 | ko login --username AWS --password-stdin public.ecr.aws
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${RELEASE_REPO}
 }
 
 buildImages() {
