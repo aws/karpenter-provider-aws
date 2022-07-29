631d2ed6c51b19a6d7e2f7266660bbbfaa2769c3
Jonathan Innis <joinnis@amazon.com>
2022-07-29 15:47:09 -0700

    Update file permissions to 0755

--- hack/setup-roles.sh
+++ hack/setup-roles.sh
@@ -1,14 +1,6 @@
 SCRIPT_DIR=website/content/en/preview/getting-started/getting-started-with-eksctl/scripts
-TEMP_DIR=$(mktemp -d)
 
-install -m 0777 "${SCRIPT_DIR}/step03-iam-cloud-formation.sh" "$TEMP_DIR/step03.sh"
-install -m 0777 "${SCRIPT_DIR}/step04-grant-access.sh" "$TEMP_DIR/step04.sh"
-install -m 0777 "$SCRIPT_DIR/step05-controller-iam.sh" "$TEMP_DIR/step05.sh"
-install -m 0777 "$SCRIPT_DIR/step06-add-spot-role.sh" "$TEMP_DIR/step06.sh"
-
-"$TEMP_DIR"/step03.sh
-"$TEMP_DIR"/step04.sh
-"$TEMP_DIR"/step05.sh
-"$TEMP_DIR"/step06.sh
-
-rm -r "$TEMP_DIR"
+"$SCRIPT_DIR"/step03-iam-cloud-formation.sh
+"$SCRIPT_DIR"/step04-grant-access.sh
+"$SCRIPT_DIR"/step05-controller-iam.sh
+"$SCRIPT_DIR"/step06-add-spot-role.sh
