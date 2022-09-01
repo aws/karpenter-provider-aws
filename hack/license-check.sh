09073f44f789459a3171f5a2f06278db79dedceb
Ryan Maleki <ryan@maleki.email>
2022-09-01 15:19:05 -0700

    fix typo

--- hack/license-check.sh
+++ hack/license-check.sh
@@ -5,6 +5,6 @@ for i in $(
   find ./cmd ./pkg ./test ./hack -name "*.go"
 ); do
   if ! grep -q "Apache License" $i; then
-    cat hack/license-boilerplacte.txt $i >$i.new && mv $i.new $i
+    cat hack/license-boilerplate.txt $i >$i.new && mv $i.new $i
   fi
 done
