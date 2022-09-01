74707c52ea79496712bc6f5131b7d3f72ecb258a
Ryan Maleki <spring1843@users.noreply.github.com>
2022-09-01 11:31:04 -0700

    chore: Automate website release changes (#2431)
    Script some of the manual changes done during release process for website deployment

--- hack/release.sh
+++ hack/release.sh
@@ -23,8 +23,39 @@ website() {
     find website/content/en/${RELEASE_VERSION}/*/*/*.yaml -type f | xargs perl -i -p -e "s/preview/${RELEASE_VERSION}/g;"
 }
 
+editWebsiteConfig() {
+  sed -i '' '/^\/docs\/\*/d' website/static/_redirects
+  echo "/docs/*     	                /${RELEASE_VERSION}/:splat" >>website/static/_redirects
+
+  yq -i ".params.latest_release_version = \"${RELEASE_VERSION}\"" website/config.yaml
+  yq -i ".menu.main[] |=select(.name == \"Docs\") .url = \"${RELEASE_VERSION}\"" website/config.yaml
+
+  editWebsiteVersionsMenu
+}
+
+# editWebsiteVersionsMenu sets relevant releases in the version dropdown menu of the website
+# without increasing the size of the set.
+# TODO: We need to maintain a list of latest minors here only. This is not currently
+# easy to achieve, however when we have a major release and we decide to maintain
+# a selected minor releases we can maintain that list in the repo and use it in here
+editWebsiteVersionsMenu() {
+  VERSIONS=(${RELEASE_VERSION})
+  while IFS= read -r LINE; do
+    SANITIZED_VERSION=$(echo "${LINE}" | sed -e 's/["-]//g' -e 's/ *//g')
+    VERSIONS+=("${SANITIZED_VERSION}")
+  done < <(yq '.params.versions' website/config.yaml)
+  unset VERSIONS[${#VERSIONS[@]}-2]
+
+  yq -i '.params.versions = []' website/config.yaml
+
+  for VERSION in "${VERSIONS[@]}"; do
+    yq -i ".params.versions += \"${VERSION}\"" website/config.yaml
+  done
+}
+
 authenticate
 buildImages $RELEASE_VERSION
 cosignImages
 chart
 website
+editWebsiteConfig
