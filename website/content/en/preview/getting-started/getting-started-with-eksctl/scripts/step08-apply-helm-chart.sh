9140876a808ad870aa0f3ad2d9cd2634ca3aaae3
Ryan Maleki <ryan@maleki.email>
2022-09-30 15:58:52 -0700

    remove extra line

--- website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step08-apply-helm-chart.sh
+++ website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step08-apply-helm-chart.sh
@@ -1,5 +1,4 @@
 helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version ${KARPENTER_VERSION} --namespace karpenter --create-namespace \
-  karpenter karpenter/karpenter \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_IAM_ROLE_ARN} \
   --set clusterName=${CLUSTER_NAME} \
   --set clusterEndpoint=${CLUSTER_ENDPOINT} \
