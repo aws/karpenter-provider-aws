075b4e5657f9cf4daa39c6504fed1a03a08b300f
Brandon <wagnerbm@amazon.com>
2022-10-14 11:15:29 -0500

    fix tests

--- pkg/cloudproviders/aws/cloudprovider/suite_test.go
+++ pkg/cloudproviders/aws/cloudprovider/suite_test.go
@@ -17,6 +17,7 @@ package cloudprovider
 import (
 	"context"
 	"math"
+	"net"
 	"testing"
 	"time"
 
@@ -174,7 +175,7 @@ var _ = BeforeEach(func() {
 	ssmCache.Flush()
 	ec2Cache.Flush()
 	instanceTypeCache.Flush()
-	cloudProvider.instanceProvider.launchTemplateProvider.kubeDNSIP = nil
+	cloudProvider.instanceProvider.launchTemplateProvider.kubeDNSIP = net.ParseIP("10.0.100.10")
 })
 
 var _ = AfterEach(func() {
