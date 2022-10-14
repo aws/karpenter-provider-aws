883584bb547de1299f8b489af2e76ee1c4f35c22
Ellis Tarn <ellistarn@gmail.com>
2022-10-14 16:03:04 -0700

    chore: rely on karpenter-core scheduling libraries (#2667)

--- pkg/cloudproviders/aws/cloudprovider/instancetype.go
+++ pkg/cloudproviders/aws/cloudprovider/instancetype.go
@@ -31,10 +31,10 @@ import (
 
 	"github.com/aws/karpenter-core/pkg/apis/provisioning/v1alpha5"
 
+	"github.com/aws/karpenter-core/pkg/scheduling"
 	"github.com/aws/karpenter/pkg/cloudproviders/aws/apis/v1alpha1"
 	"github.com/aws/karpenter/pkg/cloudproviders/aws/cloudprovider/amifamily"
 	"github.com/aws/karpenter/pkg/cloudproviders/common/cloudprovider"
-	"github.com/aws/karpenter/pkg/scheduling"
 	"github.com/aws/karpenter/pkg/utils/injection"
 	"github.com/aws/karpenter/pkg/utils/resources"
 )
