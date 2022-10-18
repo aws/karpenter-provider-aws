1391f543bcc781f6c5400983f8484454b17bacf4
Todd Neal <tnealt@amazon.com>
2022-10-17 21:14:30 -0500

    chore: add more validation to old version check

--- website/assets/js/versionWarning/index.js
+++ website/assets/js/versionWarning/index.js
@@ -1,21 +1,25 @@
 export function versionWarning() {
   const viewingVersion = window.location.pathname.split('/')[1]
-  if (viewingVersion) {
-    const latestVersion = [
-      ...document.querySelectorAll("#navbarVersionSelector .dropdown-item")
-    ].map(el => el.dataset.docsVersion)[0]
 
-    if (viewingVersion !== latestVersion) {
-      const alertElement = document.createElement('div')
-      alertElement.classList.add('alert', 'alert-warning')
-      alertElement.innerHTML = `
-        <h3>You are viewing Karpenter's <strong>${viewingVersion}</strong> documentation</h3>
-        <p>
-          Karpenter <strong>${viewingVersion}</strong> is not the latest stable release. 
-          For up-to-date documentation, see the <a href="/${latestVersion}">latest version</a>.
-        </p>
-      `
-      document.querySelector('.td-content').prepend(alertElement)
-    }
+  // only alert if we recognize the version string
+  if (!viewingVersion || !viewingVersion.match(/^v\d+\.\d+\.\d+$/)) {
+    return
+  }
+
+  const latestVersion = [
+    ...document.querySelectorAll("#navbarVersionSelector .dropdown-item")
+  ].map(el => el.dataset.docsVersion)[0]
+
+  if (viewingVersion !== latestVersion) {
+    const alertElement = document.createElement('div')
+    alertElement.classList.add('alert', 'alert-warning')
+    alertElement.innerHTML = `
+      <h3>You are viewing Karpenter's <strong>${viewingVersion}</strong> documentation</h3>
+      <p>
+        Karpenter <strong>${viewingVersion}</strong> is not the latest stable release. 
+        For up-to-date documentation, see the <a href="/${latestVersion}">latest version</a>.
+      </p>
+    `
+    document.querySelector('.td-content').prepend(alertElement)
   }
 }
