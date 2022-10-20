8a761fdb3ffc8ea00c6af46b145d0f2ec4af7d6f
Ryan Maleki <rmaleki@amazon.com>
2022-10-20 14:35:26 -0700

    * For stable releases, checckout the repo at the SHA of the tag when releasing instead of HEAD * Move release files into a release directory * Add human friendly names for ambigious steps * Move configuration values to a single place to make the developement environment easier * Seperate website prepration for releasing so that it can be run independently when troubleshooting without having to hack the scripts * Detect OS and run a different sed command for compliance between osx and linux when preparing the website * Add more details to the website PR

--- hack/release/common.sh
+++ hack/release/common.sh
@@ -1,24 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CURRENT_MAJOR_VERSION="0"
-PRIVATE_PULL_THROUGH_HOST="071440425669.dkr.ecr.us-east-1.amazonaws.com"
+config(){
+  AWS_ACCOUNT_ID="071440425669"
+  ECR_GALLERY_NAME="karpenter"
+  RELEASE_REPO=${RELEASE_REPO:-public.ecr.aws/${ECR_GALLERY_NAME}/}
+  PRIVATE_PULL_THROUGH_HOST="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
+  SNS_TOPIC_ARN="arn:aws:sns:us-east-1:${AWS_ACCOUNT_ID}:KarpenterReleases"
+  CURRENT_MAJOR_VERSION="0"
+  RELEASE_PLATFORM="--platform=linux/amd64,linux/arm64"
+}
 
-if [[ $SNAPSHOT_TAG != v* ]]; then
-  HELM_CHART_VERSION="v${CURRENT_MAJOR_VERSION}-${SNAPSHOT_TAG}"
-fi
-RELEASE_VERSION=${RELEASE_VERSION:-"${SNAPSHOT_TAG}"}
-RELEASE_PLATFORM="--platform=linux/amd64,linux/arm64"
+setEnvVariables(){
+  if [ -z ${RELEASE_VERSION+x} ];then
+    echo "Required env variable RELEASE_VERSION not set"
+    exit 1
+  fi
 
-# TODO restore https://reproducible-builds.org/docs/source-date-epoch/
-DATE_FMT="+%Y-%m-%dT%H:%M:%SZ"
-if [ -z "${SOURCE_DATE_EPOCH-}" ]; then
-    BUILD_DATE=$(date -u ${DATE_FMT})
-else
-    BUILD_DATE=$(date -u -d "${SOURCE_DATE_EPOCH}" "${DATE_FMT}" 2>/dev/null || date -u -r "${SOURCE_DATE_EPOCH}" "$(DATE_FMT)" 2>/dev/null || date -u "$(DATE_FMT)")
-fi
+  IS_STABLE_RELEASE=false
+  if [[ "${RELEASE_VERSION}" == v* ]]; then
+    IS_STABLE_RELEASE=true
+  fi
 
-COSIGN_FLAGS="-a GIT_HASH=$(git rev-parse HEAD) -a GIT_VERSION=${RELEASE_VERSION} -a BUILD_DATE=${BUILD_DATE}"
+  if [[ $IS_STABLE_RELEASE == true ]]; then
+    HELM_CHART_VERSION=$RELEASE_VERSION
+  else
+    HELM_CHART_VERSION="v${CURRENT_MAJOR_VERSION}-${RELEASE_VERSION}"
+  fi
+
+  echo "Is Stable Release? ${IS_STABLE_RELEASE}, Helm Chart Version ${HELM_CHART_VERSION} "
+
+  # TODO restore https://reproducible-builds.org/docs/source-date-epoch/
+  DATE_FMT="+%Y-%m-%dT%H:%M:%SZ"
+  if [ -z "${SOURCE_DATE_EPOCH-}" ]; then
+      BUILD_DATE=$(date -u ${DATE_FMT})
+  else
+      BUILD_DATE=$(date -u -d "${SOURCE_DATE_EPOCH}" "${DATE_FMT}" 2>/dev/null || date -u -r "${SOURCE_DATE_EPOCH}" "$(DATE_FMT)" 2>/dev/null || date -u "$(DATE_FMT)")
+  fi
+
+  COSIGN_FLAGS="-a GIT_HASH=$(git rev-parse HEAD) -a GIT_VERSION=${RELEASE_VERSION} -a BUILD_DATE=${BUILD_DATE}"
+}
 
 authenticate() {
     aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${RELEASE_REPO}
@@ -29,7 +50,6 @@ authenticatePrivateRepo() {
 }
 
 buildImages() {
-    HELM_CHART_VERSION=$1
     CONTROLLER_DIGEST=$(GOFLAGS=${GOFLAGS} KO_DOCKER_REPO=${RELEASE_REPO} ko publish -B -t ${RELEASE_VERSION} ${RELEASE_PLATFORM} ./cmd/controller)
     WEBHOOK_DIGEST=$(GOFLAGS=${GOFLAGS} KO_DOCKER_REPO=${RELEASE_REPO} ko publish -B -t ${RELEASE_VERSION} ${RELEASE_PLATFORM} ./cmd/webhook)
     yq e -i ".controller.image = \"${CONTROLLER_DIGEST}\"" charts/karpenter/values.yaml
@@ -48,7 +68,7 @@ notifyRelease() {
     RELEASE_IDENTIFIER=$2
     MESSAGE="{\"releaseType\":\"${RELEASE_TYPE}\",\"releaseIdentifier\":\"${RELEASE_IDENTIFIER}\"}"
     aws sns publish \
-        --topic-arn "arn:aws:sns:us-east-1:071440425669:KarpenterReleases" \
+        --topic-arn ${SNS_TOPIC_ARN} \
         --message ${MESSAGE} \
         --no-cli-pager
 }
@@ -74,7 +94,8 @@ publishHelmChart() {
     cd ..
 }
 
-website() {
+createNewWebsiteDirectory() {
+    RELEASE_VERSION=$1
     mkdir -p website/content/en/${RELEASE_VERSION}
     cp -r website/content/en/preview/* website/content/en/${RELEASE_VERSION}/
     find website/content/en/${RELEASE_VERSION}/ -type f | xargs perl -i -p -e "s/{{< param \"latest_release_version\" >}}/${RELEASE_VERSION}/g;"
@@ -82,15 +103,19 @@ website() {
 }
 
 editWebsiteConfig() {
-  # sed has a different syntax on mac, to do the same on mac run:
-  # sed -i '' '/^\/docs\/\*/d' website/static/_redirects
-  sed -i '/^\/docs\/\*/d' website/static/_redirects
+  RELEASE_VERSION=$1
+
+  # sed has a different syntax on mac
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' '/^\/docs\/\*/d' website/static/_redirects
+  else
+    sed -i '/^\/docs\/\*/d' website/static/_redirects
+  fi
+
   echo "/docs/*     	                /${RELEASE_VERSION}/:splat" >>website/static/_redirects
 
   yq -i ".params.latest_release_version = \"${RELEASE_VERSION}\"" website/config.yaml
   yq -i ".menu.main[] |=select(.name == \"Docs\") .url = \"${RELEASE_VERSION}\"" website/config.yaml
-
-  editWebsiteVersionsMenu
 }
 
 # editWebsiteVersionsMenu sets relevant releases in the version dropdown menu of the website
@@ -99,6 +124,7 @@ editWebsiteConfig() {
 # easy to achieve, however when we have a major release and we decide to maintain
 # a selected minor releases we can maintain that list in the repo and use it in here
 editWebsiteVersionsMenu() {
+  RELEASE_VERSION=$1
   VERSIONS=(${RELEASE_VERSION})
   while IFS= read -r LINE; do
     SANITIZED_VERSION=$(echo "${LINE}" | sed -e 's/["-]//g' -e 's/ *//g')
