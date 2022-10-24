fec82bdbb25694cfe2a0f1a06445e0b144245d25
Ellis Tarn <ellistarn@gmail.com>
2022-10-24 11:34:18 -0700

    fix: Fixed API Code Gen

--- hack/api-code-gen.sh
+++ hack/api-code-gen.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 pricing() {
-  GENERATED_FILE="pkg/cloudproviders/aws/cloudprovider/zz_generated.pricing.go"
-  NO_UPDATE=$' pkg/cloudproviders/aws/cloudprovider/zz_generated.pricing.go | 4 ++--\n 1 file changed, 2 insertions(+), 2 deletions(-)'
+  GENERATED_FILE="pkg/cloudprovider/zz_generated.pricing.go"
+  NO_UPDATE=$' pkg/cloudprovider/zz_generated.pricing.go | 4 ++--\n 1 file changed, 2 insertions(+), 2 deletions(-)'
   SUBJECT="Pricing"
 
   go run hack/code/prices_gen.go -- "${GENERATED_FILE}"
@@ -13,7 +13,7 @@ pricing() {
 }
 
 vpcLimits() {
-  GENERATED_FILE="pkg/cloudproviders/aws/cloudprovider/zz_generated.vpclimits.go"
+  GENERATED_FILE="pkg/cloudprovider/zz_generated.vpclimits.go"
   NO_UPDATE=''
   SUBJECT="VPC Limits"
 
