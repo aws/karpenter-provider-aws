9abb3d60b500d04c43ce0adfe86232f199445f48
Jonathan Innis <joinnis@amazon.com>
2022-11-04 14:33:04 -0700

    test: Fix CNI E2E tests (#2804)

--- test/suites/integration/cni_test.go
+++ test/suites/integration/cni_test.go
@@ -20,9 +20,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/aws/karpenter-core/pkg/apis/provisioning/v1alpha5"
 	"github.com/aws/karpenter-core/pkg/test"
@@ -34,7 +32,9 @@ import (
 
 var _ = Describe("CNITests", func() {
 	It("should set max pods to 110 when AWSENILimited when AWS_ENI_LIMITED_POD_DENSITY is false", func() {
-		updateEnvironment(corev1.EnvVar{Name: "AWS_ENI_LIMITED_POD_DENSITY", Value: "false"})
+		env.ExpectSettingsOverridden(map[string]string{
+			"aws.enableENILimitedPodDensity": "false",
+		})
 		provider := awstest.AWSNodeTemplate(v1alpha1.AWSNodeTemplateSpec{
 			AWS: v1alpha1.AWS{
 				SecurityGroupSelector: map[string]string{"karpenter.sh/discovery": settings.FromContext(env.Context).ClusterName},
@@ -52,9 +52,6 @@ var _ = Describe("CNITests", func() {
 		Expect(env.Client.Get(env.Context, types.NamespacedName{Name: pod.Spec.NodeName}, &node)).To(Succeed())
 		allocatablePods, _ := node.Status.Allocatable.Pods().AsInt64()
 		Expect(allocatablePods).To(Equal(int64(110)))
-		// At the end of the test, set this to true since an unset/true value is the default and what all other integration tests
-		// should rely on.
-		updateEnvironment(corev1.EnvVar{Name: "AWS_ENI_LIMITED_POD_DENSITY", Value: "true"})
 	})
 	It("should set eni-limited maxPods when AWSENILimited when AWS_ENI_LIMITED_POD_DENSITY is true", func() {
 		provider := awstest.AWSNodeTemplate(v1alpha1.AWSNodeTemplateSpec{
@@ -75,27 +72,6 @@ var _ = Describe("CNITests", func() {
 		Expect(allocatablePods).To(Equal(eniLimitedPodsFor(node.Labels["node.kubernetes.io/instance-type"])))
 	})
 })
-
-// updateEnvironment sets the provided envVar to all of Karpenter's containers.
-// It makes the assumption that Karpenter is deployed as `karpenter/karpenter`. All previously applied
-// environment variables are preserved, and this function will return once all Karpenter pods are recycled.
-func updateEnvironment(envVar corev1.EnvVar) {
-	karpenterDeployment, err := env.KubeClient.AppsV1().Deployments("karpenter").Get(env.Context, "karpenter", v1.GetOptions{})
-	Expect(err).ToNot(HaveOccurred())
-	originalDeployment := karpenterDeployment.DeepCopy()
-	karpenterPodContainers := karpenterDeployment.Spec.Template.Spec.Containers
-	for idx := range karpenterPodContainers {
-		var updatedVariables = []corev1.EnvVar{envVar}
-		for _, currentVariable := range karpenterPodContainers[idx].Env {
-			if currentVariable.Name != envVar.Name {
-				updatedVariables = append(updatedVariables, currentVariable)
-			}
-		}
-		karpenterPodContainers[idx].Env = updatedVariables
-	}
-	Expect(env.Client.Patch(env.Context, karpenterDeployment, client.MergeFrom(originalDeployment))).To(Succeed())
-	env.EventuallyExpectKarpenterWithEnvVar(envVar)
-}
 
 func eniLimitedPodsFor(instanceType string) int64 {
 	instance, err := env.EC2API.DescribeInstanceTypes(&ec2.DescribeInstanceTypesInput{
