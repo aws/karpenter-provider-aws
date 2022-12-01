82cc18cecd057e7bf218e4477b84db762da62aa3
Ellis Tarn <ellistarn@gmail.com>
2022-11-30 20:58:16 -0800

    docs: Remove deprecated versions which were vastly slowing down page loads (#2948)

--- website/content/en/v0.9.0/getting-started/getting-started-with-eksctl/scripts/step08-apply-helm-chart.sh
+++
@@ -1,8 +0,0 @@
-helm upgrade --install --namespace karpenter --create-namespace \
-  karpenter karpenter/karpenter \
-  --version ${KARPENTER_VERSION} \
-  --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_IAM_ROLE_ARN} \
-  --set clusterName=${CLUSTER_NAME} \
-  --set clusterEndpoint=${CLUSTER_ENDPOINT} \
-  --set aws.defaultInstanceProfile=KarpenterNodeInstanceProfile-${CLUSTER_NAME} \
-  --wait # for the defaulting webhook to install before creating a Provisioner
