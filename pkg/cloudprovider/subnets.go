5ea2a10e08c76218deff638446e079ce0a034041
Amanuel Engeda <aengeda@amazon.com>
2022-12-07 15:52:09 -0800

    New Comment

--- pkg/cloudprovider/subnets.go
+++ pkg/cloudprovider/subnets.go
@@ -58,6 +58,7 @@ func (p *SubnetProvider) Get(ctx context.Context, nodeTemplate *v1alpha1.AWSNode
 	if subnets, ok := p.Cache.Get(fmt.Sprint(hash)); ok {
 		return subnets.([]*ec2.Subnet), nil
 	}
+	// The section below is to alow backward compatibility for provisioner.Spec.provider
 	output, err := p.ec2api.DescribeSubnetsWithContext(ctx, &ec2.DescribeSubnetsInput{Filters: filters})
 	if err != nil {
 		return nil, fmt.Errorf("describing subnets %s, %w", pretty.Concise(filters), err)
