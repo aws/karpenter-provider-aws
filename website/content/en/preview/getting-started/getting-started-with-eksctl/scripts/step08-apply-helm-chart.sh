d38c4c7c5bb033f133d62a054028039fd328ba3b
Ryan Maleki <rmaleki@amazon.com>
2022-12-12 10:32:41 -0800

    add docker logout before helm upgrade, to avoid authentication problems

--- website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step08-apply-helm-chart.sh
+++ website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step08-apply-helm-chart.sh
@@ -1,3 +1,4 @@
+docker logout public.ecr.aws
 helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version ${KARPENTER_VERSION} --namespace karpenter --create-namespace \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_IAM_ROLE_ARN} \
   --set settings.aws.clusterName=${CLUSTER_NAME} \
