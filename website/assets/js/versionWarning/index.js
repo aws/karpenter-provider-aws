d399d3a0ee37336871a3355c12d06aed606950f7
Justin Garrison <justinleegarrison@gmail.com>
2022-12-13 16:26:13 -0800

    docs: Retain current path in new version docs link (#3013)
    Co-authored-by: Ryan Maleki <spring1843@users.noreply.github.com>

--- website/assets/js/versionWarning/index.js
+++ website/assets/js/versionWarning/index.js
@@ -11,13 +11,15 @@ export function versionWarning() {
   ].map(el => el.dataset.docsVersion)[0]
 
   if (viewingVersion !== latestVersion) {
+    // keep the current path if user is viewing and old version of the docs
+    const newPath = window.location.pathname.replace(viewingVersion, latestVersion).replace(/^\//g, '')
     const alertElement = document.createElement('div')
     alertElement.classList.add('alert', 'alert-warning')
     alertElement.innerHTML = `
       <h3>You are viewing Karpenter's <strong>${viewingVersion}</strong> documentation</h3>
       <p>
         Karpenter <strong>${viewingVersion}</strong> is not the latest stable release. 
-        For up-to-date documentation, see the <a href="/${latestVersion}">latest version</a>.
+        For up-to-date documentation, see the <a href="/${newPath}">latest version</a>.
       </p>
     `
     document.querySelector('.td-content').prepend(alertElement)
