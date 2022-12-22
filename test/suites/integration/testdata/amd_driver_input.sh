56077d2947693deefa6dd608d80cf0fed839cd20
Jonathan Innis <joinnis@amazon.com>
2022-12-22 10:58:33 -0600

    test: Run reboot after running the EKS bootstrap script (#3103)

--- test/suites/integration/testdata/amd_driver_input.sh
+++ test/suites/integration/testdata/amd_driver_input.sh
@@ -8,6 +8,8 @@ Content-Type: text/x-shellscript; charset="us-ascii"
 cd
 sudo amazon-linux-extras install epel -y
 sudo yum update -y
+
+# Create a script to install the AMD Radeon GPU
 cat << EOF > /tmp/amd-install.sh
 #!/bin/bash
 export echo PATH=/usr/local/bin:$PATH
@@ -19,6 +21,8 @@ systemctl disable amd-install.service
 reboot
 EOF
 sudo chmod +x /tmp/amd-install.sh
+
+# Create a service that will run on system reboot
 cat << EOF > /etc/systemd/system/amd-install.service
 [Unit]
 Description=install amd drivers
@@ -30,5 +34,13 @@ ExecStart=/bin/bash /tmp/amd-install.sh
 WantedBy=multi-user.target
 EOF
 sudo systemctl enable amd-install.service
-(sleep 60; sudo reboot) &
+
+# Run the EKS bootstrap script and then reboot
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+/etc/eks/bootstrap.sh '%s' --apiserver-endpoint '%s' --b64-cluster-ca '%s' \
+--use-max-pods false \
+--container-runtime containerd \
+--kubelet-extra-args '--node-labels=karpenter.sh/provisioner-name=%s,testing.karpenter.sh/test-id=unspecified'
+
+reboot
 --BOUNDARY-- 
