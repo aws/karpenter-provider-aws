4460cbbf03fb02039170b0a556c4275b0bbc7366
Ryan Maleki <rmaleki@amazon.com>
2022-12-27 13:46:05 -0800

    Look for tag and fail for releasing website

--- hack/release/prepare-website.sh
+++ hack/release/prepare-website.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#!/usr/bin/env bash
-set -euo pipefail
-
-GIT_TAG=$(git describe --exact-match --tags || echo "none")
-if [ -z ${RELEASE_VERSION+x} ];then
-  echo "Missing required git tag"
-  exit 1
-fi
-
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/common.sh"
 
+config
+
+GIT_TAG=$(git describe --exact-match --tags || echo "none")
+if [[ $(releaseType $GIT_TAG) != $RELEASE_TYPE_STABLE ]]; then
+  echo "Not a stable release. Missing required git tag."
+  exit 1
+fi
 echo "RenderingPrep website files for ${GIT_TAG}"
 
 createNewWebsiteDirectory $GIT_TAG
