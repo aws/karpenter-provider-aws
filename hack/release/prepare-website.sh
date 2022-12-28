5d615349b0af3465b8107066495eab014bcc1518
Ryan Maleki <spring1843@users.noreply.github.com>
2022-12-28 11:37:16 -0800

    Fix: stable release by looking at git tags instead of expecting a parameter (#3116)

--- hack/release/prepare-website.sh
+++ hack/release/prepare-website.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
- if [ -z ${RELEASE_VERSION+x} ];then
-    echo "missing required environment variable RELEASE_VERSION"
-    exit 1
-  fi
-
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/common.sh"
 
-echo "RenderingPrep website files for ${RELEASE_VERSION}"
+config
 
-createNewWebsiteDirectory $RELEASE_VERSION
-editWebsiteConfig $RELEASE_VERSION
-editWebsiteVersionsMenu $RELEASE_VERSION
+GIT_TAG=$(git describe --exact-match --tags || echo "none")
+if [[ $(releaseType $GIT_TAG) != $RELEASE_TYPE_STABLE ]]; then
+  echo "Not a stable release. Missing required git tag."
+  exit 1
+fi
+echo "RenderingPrep website files for ${GIT_TAG}"
+
+createNewWebsiteDirectory $GIT_TAG
+editWebsiteConfig $GIT_TAG
+editWebsiteVersionsMenu $GIT_TAG
