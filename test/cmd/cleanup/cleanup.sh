58f40f0f571a6093a8dd333f653ef26ed0187014
Todd Neal <tnealt@amazon.com>
2023-01-17 12:11:14 -0600

    chore: update cleanup script to remove old launch templates (#3221)

--- test/cmd/cleanup/cleanup.sh
+++ test/cmd/cleanup/cleanup.sh
@@ -10,3 +10,13 @@ old_instances=$(echo -n "${all}\n${kit_infra}" | sort | uniq -c | tr -d " " | gr
 aws ec2 terminate-instances --instance-ids ${old_instances}
 
 aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE ROLLBACK_FAILED ROLLBACK_COMPLETE DELETE_FAILED UPDATE_COMPLETE UPDATE_FAILED UPDATE_ROLLBACK_FAILED UPDATE_ROLLBACK_COMPLETE IMPORT_COMPLETE IMPORT_ROLLBACK_FAILED IMPORT_ROLLBACK_COMPLETE | jq -r '.StackSummaries[] .StackName' | grep 'karpenter-tests-*-*' | xargs -n 1 -t aws cloudformation delete-stack --stack-name
+
+
+# Clean up old launch templates
+launch_templates=$(aws ec2 describe-launch-templates |  jq -r '.LaunchTemplates[].LaunchTemplateName' | grep Karpenter-karpenter-tests)
+n_lts=$(echo ${launch_templates} | wc -w)
+echo "Removing ${n_lts} launch templates"
+for lt in ${launch_templates}; do
+  echo "Deleting LT ${lt}"
+  aws ec2 delete-launch-template --no-cli-pager --launch-template-name ${lt}
+done
