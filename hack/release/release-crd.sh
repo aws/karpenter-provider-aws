e451f911a90a461e2a5ad50091b2202cb90fe80f
Ryan Maleki <spring1843@users.noreply.github.com>
2023-01-23 23:10:10 -0800

    Revert "feat: Add CRD charts as a dependency (#3260)"
    This reverts commit 57a8813d0aa9c8a649973876ee70f80c8af3afb7.

--- hack/release/release-crd.sh
+++ hack/release/release-crd.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/common.sh"
 
 config
-publishHelmChart "karpenter-crd" "${HEAD_HASH}" "${RELEASE_REPO_GH}"
+publishHelmChartToGHCR "karpenter-crd" "${HEAD_HASH}"
 
 if [[ $(releaseType $GIT_TAG) == $RELEASE_TYPE_STABLE ]]; then
-  publishHelmChart "karpenter-crd" "${GIT_TAG}" "${RELEASE_REPO_GH}"
+  publishHelmChartToGHCR "karpenter-crd" "${GIT_TAG}"
 fi
