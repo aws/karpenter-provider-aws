8fd83d05734b9588ae877cbce77e0ae69a9bf980
Christopher Negus <striker57@gmail.com>
2023-01-31 17:51:03 +0000

    Small fix

--- website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step01-config.sh
+++ website/content/en/preview/getting-started/getting-started-with-eksctl/scripts/step01-config.sh
@@ -1,4 +1,3 @@
-export KARPENTER_VERSION={{< param "latest_release_version" >}}
 export CLUSTER_NAME="${USER}-karpenter-demo"
 export AWS_DEFAULT_REGION="us-west-2"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
