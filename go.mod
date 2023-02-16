d015ec53653849880909b02a578b037e0ece1c26
Will Thames <wthames@skedulo.com>
2023-02-16 11:02:23 +1000

    Migrate tests from testify to ginkgo/gomega

--- go.mod
+++ go.mod
@@ -16,7 +16,6 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6
 	github.com/prometheus/client_golang v1.14.0
 	github.com/samber/lo v1.37.0
-	github.com/stretchr/testify v1.8.1
 	go.uber.org/multierr v1.9.0
 	go.uber.org/zap v1.24.0
 	k8s.io/api v0.25.4
@@ -74,7 +73,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
--- pkg/cloudprovider/amifamily/ami_test.go
+++ pkg/cloudprovider/amifamily/ami_test.go
@@ -6,122 +6,125 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
+
+func TestAWS(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AMISelector")
+}
 
 var (
 	defaultOwners = []*string{aws.String("self"), aws.String("amazon")}
 )
 
-func TestNoOwnersNoPrefix(t *testing.T) {
-	amiSelector := map[string]string{
-		"Name": "my-ami",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, defaultOwners)
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("tag:Name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
+var _ = Describe("AMI Selectors", func() {
+	It("should have default owners and use tags when prefixes aren't set", func() {
+		amiSelector := map[string]string{
+			"Name": "my-ami",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(ConsistOf(defaultOwners))
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("tag:Name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+		}))
 	})
-}
-
-func TestNoOwnersPrefixedName(t *testing.T) {
-	amiSelector := map[string]string{
-		"aws::name": "my-ami",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, defaultOwners)
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
+	It("should have default owners and use name when prefixed", func() {
+		amiSelector := map[string]string{
+			"aws::name": "my-ami",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(ConsistOf(defaultOwners))
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+		}))
 	})
-}
-
-func TestLegacyIdsPrefixedName(t *testing.T) {
-	amiSelector := map[string]string{
-		"aws::name": "my-ami",
-		"aws-ids":   "ami-abcd1234,ami-cafeaced",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, nil)
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
-		{
-			Name: aws.String("image-id"),
-			Values: aws.StringSlice([]string{
-				"ami-abcd1234",
-				"ami-cafeaced",
-			}),
-		},
+	It("should not set owners when legacy ids are passed", func() {
+		amiSelector := map[string]string{
+			"aws::name": "my-ami",
+			"aws-ids":   "ami-abcd1234,ami-cafeaced",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(BeNil())
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+			{
+				Name: aws.String("image-id"),
+				Values: aws.StringSlice([]string{
+					"ami-abcd1234",
+					"ami-cafeaced",
+				}),
+			},
+		}))
 	})
-}
-
-func TestIdsPrefixedName(t *testing.T) {
-	amiSelector := map[string]string{
-		"aws::name": "my-ami",
-		"aws::ids":  "ami-abcd1234,ami-cafeaced",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, nil)
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
-		{
-			Name: aws.String("image-id"),
-			Values: aws.StringSlice([]string{
-				"ami-abcd1234",
-				"ami-cafeaced",
-			}),
-		},
+	It("should not set owners when prefixed ids are passed", func() {
+		amiSelector := map[string]string{
+			"aws::name": "my-ami",
+			"aws::ids":  "ami-abcd1234,ami-cafeaced",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(BeNil())
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+			{
+				Name: aws.String("image-id"),
+				Values: aws.StringSlice([]string{
+					"ami-abcd1234",
+					"ami-cafeaced",
+				}),
+			},
+		}))
 	})
-}
-
-func TestIdsPrefixedNameWithOwners(t *testing.T) {
-	amiSelector := map[string]string{
-		"aws::name":   "my-ami",
-		"aws::ids":    "ami-abcd1234,ami-cafeaced",
-		"aws::owners": "self,amazon",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, defaultOwners)
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
-		{
-			Name: aws.String("image-id"),
-			Values: aws.StringSlice([]string{
-				"ami-abcd1234",
-				"ami-cafeaced",
-			}),
-		},
+	It("should have work with prefixed id and prefixed name and owners", func() {
+		amiSelector := map[string]string{
+			"aws::name":   "my-ami",
+			"aws::ids":    "ami-abcd1234,ami-cafeaced",
+			"aws::owners": "self,amazon",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(ConsistOf(defaultOwners))
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+			{
+				Name: aws.String("image-id"),
+				Values: aws.StringSlice([]string{
+					"ami-abcd1234",
+					"ami-cafeaced",
+				}),
+			},
+		}))
 	})
-}
-
-func TestOwnersPrefixedName(t *testing.T) {
-	amiSelector := map[string]string{
-		"aws::name":   "my-ami",
-		"aws::owners": "0123456789,self",
-	}
-	filters, owners := getFiltersAndOwners(amiSelector)
-	assert.ElementsMatch(t, owners, []*string{
-		aws.String("0123456789"),
-		aws.String("self"),
+	It("should have work with prefixed name and owners", func() {
+		amiSelector := map[string]string{
+			"aws::name":   "my-ami",
+			"aws::owners": "0123456789,self",
+		}
+		filters, owners := getFiltersAndOwners(amiSelector)
+		Expect(owners).Should(ConsistOf([]*string{
+			aws.String("0123456789"),
+			aws.String("self"),
+		}))
+		Expect(filters).Should(ConsistOf([]*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{"my-ami"}),
+			},
+		}))
 	})
-	assert.ElementsMatch(t, filters, []*ec2.Filter{
-		{
-			Name:   aws.String("name"),
-			Values: aws.StringSlice([]string{"my-ami"}),
-		},
-	})
-}
+})
