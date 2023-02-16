125d2dd67f64f23647e2bf6b075c019c274cd990
Amanuel Engeda <aengeda@amazon.com>
2023-02-15 16:38:45 -0800

    Clean-up

--- test/suites/integration/daemonset_test.go
+++ test/suites/integration/daemonset_test.go
@@ -15,6 +15,7 @@ limitations under the License.
 package integration_test
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -30,16 +31,20 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 )
 
+var provider *v1alpha1.AWSNodeTemplate
+var provisioner *v1alpha5.Provisioner
 var limitrange *v1.LimitRange
 var priorityclass *schedulingv1.PriorityClass
+var daemonset *appsv1.DaemonSet
+var dep *appsv1.Deployment
 
 var _ = Describe("DaemonSet", func() {
-	It("should account for LimitRange Containers Default For Resources", func() {
-		provider := awstest.AWSNodeTemplate(v1alpha1.AWSNodeTemplateSpec{AWS: v1alpha1.AWS{
+	BeforeEach(func() {
+		provider = awstest.AWSNodeTemplate(v1alpha1.AWSNodeTemplateSpec{AWS: v1alpha1.AWS{
 			SecurityGroupSelector: map[string]string{"karpenter.sh/discovery": settings.FromContext(env.Context).ClusterName},
 			SubnetSelector:        map[string]string{"karpenter.sh/discovery": settings.FromContext(env.Context).ClusterName},
 		}})
-		provisioner := test.Provisioner(test.ProvisionerOptions{
+		provisioner = test.Provisioner(test.ProvisionerOptions{
 			ProviderRef: &v1alpha5.ProviderRef{Name: provider.Name},
 		})
 		preemptNever := v1.PreemptNever
@@ -66,14 +71,14 @@ var _ = Describe("DaemonSet", func() {
 					}}},
 			},
 		}
-		daemonset := test.DaemonSet(test.DaemonSetOptions{
+		daemonset = test.DaemonSet(test.DaemonSetOptions{
 			PodOptions: test.PodOptions{
 				ResourceRequirements: v1.ResourceRequirements{Limits: v1.ResourceList{v1.ResourceMemory: resource.MustParse("1Gi")}},
 				PriorityClassName:    "high-priority-daemonsets",
 			},
 		})
 		numPods := 1
-		dep := test.Deployment(test.DeploymentOptions{
+		dep = test.Deployment(test.DeploymentOptions{
 			Replicas: int32(numPods),
 			PodOptions: test.PodOptions{
 				ObjectMeta: metav1.ObjectMeta{
@@ -84,7 +89,8 @@ var _ = Describe("DaemonSet", func() {
 				},
 			},
 		})
-
+	})
+	It("should account for LimitRange Containers Default For Resources", func() {
 		podSelector := labels.SelectorFromSet(dep.Spec.Selector.MatchLabels)
 		daemonsetSector := labels.SelectorFromSet(daemonset.Spec.Selector.MatchLabels)
 		env.ExpectCreatedNodeCount("==", 0)
@@ -94,56 +100,6 @@ var _ = Describe("DaemonSet", func() {
 		env.ExpectCreatedNodeCount("==", 2)
 	})
 	It("should account for LimitRange Containers Default Requests For Resources", func() {
-		provider := awstest.AWSNodeTemplate(v1alpha1.AWSNodeTemplateSpec{AWS: v1alpha1.AWS{
-			SecurityGroupSelector: map[string]string{"karpenter.sh/discovery": settings.FromContext(env.Context).ClusterName},
-			SubnetSelector:        map[string]string{"karpenter.sh/discovery": settings.FromContext(env.Context).ClusterName},
-		}})
-		provisioner := test.Provisioner(test.ProvisionerOptions{
-			ProviderRef: &v1alpha5.ProviderRef{Name: provider.Name},
-		})
-		preemptNever := v1.PreemptNever
-		priorityclass = &schedulingv1.PriorityClass{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "high-priority-daemonsets",
-			},
-			PreemptionPolicy: &preemptNever,
-			Value:            int32(10000000),
-			GlobalDefault:    false,
-			Description:      "This priority class should be used for daemonsets.",
-		}
-		limitrange = &v1.LimitRange{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "limitrange",
-				Namespace: "default",
-			},
-			Spec: v1.LimitRangeSpec{
-				Limits: []v1.LimitRangeItem{{
-					Type: v1.LimitTypeContainer,
-					DefaultRequest: v1.ResourceList{
-						v1.ResourceCPU:    resource.MustParse("2"),
-						v1.ResourceMemory: resource.MustParse("1Gi"),
-					}}},
-			},
-		}
-		daemonset := test.DaemonSet(test.DaemonSetOptions{
-			PodOptions: test.PodOptions{
-				ResourceRequirements: v1.ResourceRequirements{Limits: v1.ResourceList{v1.ResourceMemory: resource.MustParse("1Gi")}},
-				PriorityClassName:    "high-priority-daemonsets",
-			},
-		})
-		numPods := 1
-		dep := test.Deployment(test.DeploymentOptions{
-			Replicas: int32(numPods),
-			PodOptions: test.PodOptions{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{"app": "large-app"},
-				},
-				ResourceRequirements: v1.ResourceRequirements{
-					Requests: v1.ResourceList{v1.ResourceMemory: resource.MustParse("4")},
-				},
-			},
-		})
-
 		podSelector := labels.SelectorFromSet(dep.Spec.Selector.MatchLabels)
 		daemonsetSector := labels.SelectorFromSet(daemonset.Spec.Selector.MatchLabels)
 		env.ExpectCreatedNodeCount("==", 0)
