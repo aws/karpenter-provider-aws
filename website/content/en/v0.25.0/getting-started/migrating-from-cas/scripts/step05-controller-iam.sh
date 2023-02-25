2ee3f76bbcb15af76e58d47e87179f907e359af3
Brandon <wagnerbm@amazon.com>
2023-02-25 12:46:56 -0600

    fix migrating from cas permissions

--- website/content/en/v0.25.0/getting-started/migrating-from-cas/scripts/step05-controller-iam.sh
+++ website/content/en/v0.25.0/getting-started/migrating-from-cas/scripts/step05-controller-iam.sh
@@ -1,31 +1,33 @@
-echo "{
-    \"Version\": \"2012-10-17\",
-    \"Statement\": [
+cat << EOF > controller-trust-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
         {
-            \"Effect\": \"Allow\",
-            \"Principal\": {
-                \"Federated\": \"arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_ENDPOINT#*//}\"
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_ENDPOINT#*//}"
             },
-            \"Action\": \"sts:AssumeRoleWithWebIdentity\",
-            \"Condition\": {
-                \"StringEquals\": {
-                    \"${OIDC_ENDPOINT#*//}:aud\": \"sts.amazonaws.com\",
-                    \"${OIDC_ENDPOINT#*//}:sub\": \"system:serviceaccount:karpenter:karpenter\"
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "${OIDC_ENDPOINT#*//}:aud": "sts.amazonaws.com",
+                    "${OIDC_ENDPOINT#*//}:sub": "system:serviceaccount:karpenter:karpenter"
                 }
             }
         }
     ]
-}" > controller-trust-policy.json
+}
+EOF
 
 aws iam create-role --role-name KarpenterControllerRole-${CLUSTER_NAME} \
     --assume-role-policy-document file://controller-trust-policy.json
 
-echo '{
+cat << EOF > controller-policy.json
+{
     "Statement": [
         {
             "Action": [
                 "ssm:GetParameter",
-                "iam:PassRole",
                 "ec2:DescribeImages",
                 "ec2:RunInstances",
                 "ec2:DescribeSubnets",
@@ -56,10 +58,21 @@ echo '{
             "Effect": "Allow",
             "Resource": "*",
             "Sid": "ConditionalEC2Termination"
+        },
+        {
+            "Action": "iam:PassRole",
+            "Resource": "arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT_ID}:role/KarpenterNodeRole-${CLUSTER_NAME}",
+            "Sid": "PassNodeIAMRole"
+        },
+        {
+            "Action": "eks:DescribeCluster",
+            "Resource": "arn:${AWS_PARTITION}:eks:${AWS_REGION}:${AWS_ACCOUNT_ID}:cluster/${CLUSTER_NAME}",
+            "Sid": "EKSClusterEndpointLookup"
         }
     ],
     "Version": "2012-10-17"
-}' > controller-policy.json
+}
+EOF
 
 aws iam put-role-policy --role-name KarpenterControllerRole-${CLUSTER_NAME} \
     --policy-name KarpenterControllerPolicy-${CLUSTER_NAME} \
