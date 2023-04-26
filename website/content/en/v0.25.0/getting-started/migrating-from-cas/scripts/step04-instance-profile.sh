242a256c83234700182da37bb6b26d78f0a5c662
Jonathan Innis <joinnis@amazon.com>
2023-04-26 15:04:55 -0700

    Fix folder cleanup for new versions of the website

--- website/content/en/v0.25.0/getting-started/migrating-from-cas/scripts/step04-instance-profile.sh
+++
@@ -1,6 +0,0 @@
-aws iam create-instance-profile \
-    --instance-profile-name "KarpenterNodeInstanceProfile-${ClusterName}"
-
-aws iam add-role-to-instance-profile \
-    --instance-profile-name "KarpenterNodeInstanceProfile-${ClusterName}" \
-    --role-name "KarpenterNodeRole-${CLUSTER_NAME}"
