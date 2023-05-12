f928515ae77f6a01842388fbab45d5456335c40e
njtran <njtran@amazon.com>
2023-05-12 14:18:46 -0700

    fix: undefined blockdevicemappings volumesize would panic if not defaulted

--- pkg/providers/instancetype/types.go
+++ pkg/providers/instancetype/types.go
@@ -173,12 +173,15 @@ func ephemeralStorage(amiFamily amifamily.AMIFamily, blockDeviceMappings []*v1al
 	if len(blockDeviceMappings) != 0 {
 		switch amiFamily.(type) {
 		case *amifamily.Custom:
-			return blockDeviceMappings[len(blockDeviceMappings)-1].EBS.VolumeSize
+			volSize := blockDeviceMappings[len(blockDeviceMappings)-1].EBS.VolumeSize
+			if volSize != nil {
+				return volSize
+			}
 		default:
 			ephemeralBlockDevice := amiFamily.EphemeralBlockDevice()
 			for _, blockDevice := range blockDeviceMappings {
 				// If a block device mapping exists in the provider for the root volume, set the volume size specified in the provider
-				if *blockDevice.DeviceName == *ephemeralBlockDevice {
+				if *blockDevice.DeviceName == *ephemeralBlockDevice && blockDevice.EBS.VolumeSize != nil {
 					return blockDevice.EBS.VolumeSize
 				}
 			}
