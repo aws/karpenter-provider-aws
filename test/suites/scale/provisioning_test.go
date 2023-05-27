18af73344cecbedcffb36345db15367742064666
Nick Tran <nichotr@amazon.com>
2023-05-26 17:28:27 -0700

    test: fix cloudwatch importing with missing env variable (#3950)
    Co-authored-by: njtran <njtran@amazon.com>

--- test/suites/scale/provisioning_test.go
+++ test/suites/scale/provisioning_test.go
@@ -115,7 +115,7 @@ var _ = Describe("Provisioning", Label(debug.NoWatch), func() {
 			env.EventuallyExpectHealthyPodCount(selector, replicas)
 		}, aws.ProvisioningEventType, testGroup, "pod-dense", aws.GenerateTestDimensions(expectedNodeCount, 0, replicasPerNode))
 	}, SpecTimeout(time.Minute*30))
-	It("should scale successfully on a pod-dense scale-up", func(_ context.Context) {
+	It("should scale successfully on a pod-dense scale-up", Label(debug.NoEvents), func(_ context.Context) {
 		replicasPerNode := 110
 		maxPodDensity := replicasPerNode + dsCount
 		expectedNodeCount := 60
