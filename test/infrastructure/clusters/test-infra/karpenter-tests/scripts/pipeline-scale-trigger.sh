732745a1e2d9b35c490bd4e7b68a671ea02f566a
Jonathan Innis <joinnis@amazon.com>
2023-05-31 15:37:02 -0700

    Add periodic trigger for scale testing

---
+++ test/infrastructure/clusters/test-infra/karpenter-tests/scripts/pipeline-scale-trigger.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Create a pipeline run for the scale test suite
+cat <<EOF | kubectl create -f -
+ apiVersion: tekton.dev/v1beta1
+ kind: PipelineRun
+ metadata:
+   generateName: "scale-periodic-"
+   namespace: karpenter-tests
+ spec:
+   timeouts:
+    pipeline: "4h"
+   params:
+   - name: test-filter
+     value: "Scale"
+   pipelineRef:
+     name: suite
+   serviceAccountName: karpenter-tests
+EOF
