605286b9ba1a8cb4a358441b2a86ce64f15fb705
Ellis Tarn <ellistarn@gmail.com>
2023-06-21 10:44:27 -0700

    doc: Remove CAS migration guide, as it was very similar to the other guide and kept drifting, causing confusion

--- website/content/en/preview/getting-started/migrating-from-cas/scripts/step13-scale-multiple-ng.sh
+++
@@ -1,5 +0,0 @@
-for NODEGROUP in $(aws eks list-nodegroups --cluster-name ${CLUSTER_NAME} \
-    --query 'nodegroups' --output text); do aws eks update-nodegroup-config --cluster-name ${CLUSTER_NAME} \
-    --nodegroup-name ${NODEGROUP} \
-    --scaling-config "minSize=1,maxSize=1,desiredSize=1"
-done
