cc519af241c8a84169b7f0be923c55dc60c7648e
Jason Deal <jmdeal@amazon.com>
2023-10-05 09:30:57 -0700

    test: move snapshot releases to separate ECR repo (#4734)
    Co-authored-by: Jonathan Innis <jonathan.innis.ji@gmail.com>

--- hack/release/snapshot.sh
+++ hack/release/snapshot.sh
@@ -2,15 +2,10 @@
 set -euo pipefail
 
 GIT_TAG=$(git describe --exact-match --tags || echo "no tag")
-if [[ "$GIT_TAG" == "no tag" ]]; then
-    echo "Failed to release: commit is untagged"
-    exit 1
-fi
 HEAD_HASH=$(git rev-parse HEAD)
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/common.sh"
 config
 
-release "$GIT_TAG"
-
+snapshot "$HEAD_HASH"
