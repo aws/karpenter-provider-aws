52e3a455c51cb02bdd00b099cc9029ae40e091a1
Jason Deal <jmdeal@amazon.com>
2023-11-10 13:00:27 -0800

    ci fix

--- hack/toolchain.sh
+++ hack/toolchain.sh
@@ -12,14 +12,14 @@ main() {
 tools() {
     go install github.com/google/go-licenses@v1.6.0
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.1
-    go install github.com/google/ko@v0.11.2
+    go install github.com/google/ko@latest
     go install github.com/mikefarah/yq/v4@v4.30.8
     go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.11.0
     go install sigs.k8s.io/controller-runtime/tools/setup-envtest@v0.0.0-20230213155330-c98b7fd9183a
     go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.11.3
     go install github.com/sigstore/cosign/cmd/cosign@v1.13.1
     go install -tags extended github.com/gohugoio/hugo@v0.110.0
-    go install golang.org/x/vuln/cmd/govulncheck@v0.0.0-20230217165152-67742527d09b
+    go install golang.org/x/vuln/cmd/govulncheck@latest
     go install github.com/onsi/ginkgo/v2/ginkgo@latest
 
     if ! echo "$PATH" | grep -q "${GOPATH:-undefined}/bin\|$HOME/go/bin"; then
