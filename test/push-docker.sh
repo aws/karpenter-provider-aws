41177a195b7b7dcbedc11dea5139a463d96b6f9d
Aman Agrawal <amanagr04@gmail.com>
2023-11-18 16:57:45 -0500

    fixed tag

--- test/push-docker.sh
+++ test/push-docker.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 # aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build . --progress plain -t ${DOCKER_REPOSITORY}:latest
+docker build . --progress plain -t "${DOCKER_REPOSITORY}:latest"
 docker push ${DOCKER_REPOSITORY}:latest
