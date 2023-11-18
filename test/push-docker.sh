6050ff99534cdae3e80ca93c2b7978c3373895fd
Aman Agrawal <amanagr04@gmail.com>
2023-11-18 16:26:58 -0500

    changed docker push script

--- test/push-docker.sh
+++ test/push-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build . --progress plain -t public.ecr.aws/karpenter/tools:latest
-docker push public.ecr.aws/karpenter/tools:latest
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+docker build . --progress plain -t ${DOCKER_REPOSITORY}:latest
+docker push ${DOCKER_REPOSITORY}:latest
