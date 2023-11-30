engedaam/karpenter

Add Resource count to Timestream

test/hack/resource-management/resourcetypes/instanceprofile.go
@@ -64,6 +64,15 @@ func (ip *InstanceProfile) GetExpired(ctx context.Context, expirationTime time.T
 	return names, multierr.Combine(errs...)
 }
 
+func (ip *InstanceProfile) GetCount(ctx context.Context) (count int, err error) {
+	out, err := ip.iamClient.ListInstanceProfiles(ctx, &iam.ListInstanceProfilesInput{})
+	if err != nil {
+		return count, err
+	}
+
+	return len(out.InstanceProfiles), nil
+}
+
 func (ip *InstanceProfile) Get(ctx context.Context, clusterName string) (names []string, err error) {
 	out, err := ip.iamClient.ListInstanceProfiles(ctx, &iam.ListInstanceProfilesInput{})
 	if err != nil {

test/hack/resource-management/resourcetypes/oidc.go
@@ -61,6 +61,15 @@ func (o *OIDC) GetExpired(ctx context.Context, expirationTime time.Time) (names 
 	return names, multierr.Combine(errs...)
 }
 
+func (o *OIDC) GetCount(ctx context.Context) (count int, err error) {
+	out, err := o.iamClient.ListOpenIDConnectProviders(ctx, &iam.ListOpenIDConnectProvidersInput{})
+	if err != nil {
+		return count, err
+	}
+
+	return len(out.OpenIDConnectProviderList), nil
+}
+
 func (o *OIDC) Get(ctx context.Context, clusterName string) (names []string, err error) {
 	return names, err
 }

aws/karpenter-provider-aws

test/hack/resource/pkg/resourcetypes/instanceprofile.go
@@ -64,6 +64,15 @@ func (ip *InstanceProfile) GetExpired(ctx context.Context, expirationTime time.T
 	return names, multierr.Combine(errs...)
 }
 
+func (ip *InstanceProfile) CountAll(ctx context.Context) (count int, err error) {
+	out, err := ip.iamClient.ListInstanceProfiles(ctx, &iam.ListInstanceProfilesInput{})
+	if err != nil {
+		return count, err
+	}
+
+	return len(out.InstanceProfiles), nil
+}
+
 func (ip *InstanceProfile) Get(ctx context.Context, clusterName string) (names []string, err error) {
 	out, err := ip.iamClient.ListInstanceProfiles(ctx, &iam.ListInstanceProfilesInput{})
 	if err != nil {

test/hack/resource/pkg/resourcetypes/oidc.go
@@ -61,6 +61,15 @@ func (o *OIDC) GetExpired(ctx context.Context, expirationTime time.Time) (names 
 	return names, multierr.Combine(errs...)
 }
 
+func (o *OIDC) CountAll(ctx context.Context) (count int, err error) {
+	out, err := o.iamClient.ListOpenIDConnectProviders(ctx, &iam.ListOpenIDConnectProvidersInput{})
+	if err != nil {
+		return count, err
+	}
+
+	return len(out.OpenIDConnectProviderList), nil
+}
+
 func (o *OIDC) Get(ctx context.Context, clusterName string) (names []string, err error) {
 	return names, err
 }