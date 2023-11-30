4e73f04a2406da375dd9fb43b32e5880b3d402ae
Aman Agrawal <amanagr04@gmail.com>
2023-11-29 20:36:11 -0500

    fixed merge

--- test/push-docker.sh
+++ test/push-docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build . --progress plain -t ${DOCKER_REPOSITORY}:latest
-docker push ${DOCKER_REPOSITORY}:latest
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+docker build . --progress plain -t public.ecr.aws/karpenter/tools:latest
+docker push public.ecr.aws/karpenter/tools:latest
