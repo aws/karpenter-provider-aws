c36701e8c15c80c7305587232a146c4781e42355
Jonathan Innis <joinnis@amazon.com>
2024-01-01 16:54:18 -0600

    Fix codegen diff check for timestamp

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -9,13 +9,11 @@ echo "codegen running ENABLE_GIT_PUSH: ${ENABLE_GIT_PUSH}"
 
 bandwidth() {
   GENERATED_FILE="pkg/providers/instancetype/zz_generated.bandwidth.go"
-  NO_UPDATE=''
   SUBJECT="Bandwidth"
 
   go run hack/code/bandwidth_gen/main.go -- "${GENERATED_FILE}"
 
-  GIT_DIFF=$(git diff --stat "${GENERATED_FILE}")
-  checkForUpdates "${GIT_DIFF}" "${NO_UPDATE}" "${SUBJECT}" "${GENERATED_FILE}"
+  checkForUpdates "${SUBJECT}" "${GENERATED_FILE}"
 }
 
 pricing() {
@@ -27,57 +25,57 @@ pricing() {
 
   for partition in "${PARTITIONS[@]}"; do
     GENERATED_FILE="pkg/providers/pricing/zz_generated.pricing_${partition//-/_}.go"
-    NO_UPDATE=" ${GENERATED_FILE} "$'| 4 ++--\n 1 file changed, 2 insertions(+), 2 deletions(-)'
     SUBJECT="Pricing"
 
     go run hack/code/prices_gen/main.go --partition "$partition" --output "$GENERATED_FILE"
 
-    GIT_DIFF=$(git diff --stat "${GENERATED_FILE}")
-    checkForUpdates "${GIT_DIFF}" "${NO_UPDATE}" "${SUBJECT} beside timestamps since last update" "${GENERATED_FILE}"
+    IGNORE_PATTERN="// generated at"
+    checkForUpdates "${SUBJECT} beside timestamps since last update" "${GENERATED_FILE}" "${IGNORE_PATTERN}"
   done
 }
 
 vpcLimits() {
   GENERATED_FILE="pkg/providers/instancetype/zz_generated.vpclimits.go"
-  NO_UPDATE=''
   SUBJECT="VPC Limits"
 
   go run hack/code/vpc_limits_gen/main.go -- \
     --url=https://raw.githubusercontent.com/aws/amazon-vpc-resource-controller-k8s/master/pkg/aws/vpc/limits.go \
     --output="${GENERATED_FILE}"
 
-  GIT_DIFF=$(git diff --stat "${GENERATED_FILE}")
-  checkForUpdates "${GIT_DIFF}" "${NO_UPDATE}" "${SUBJECT}" "${GENERATED_FILE}"
+  checkForUpdates "${SUBJECT}" "${GENERATED_FILE}"
 }
 
 instanceTypeTestData() {
   GENERATED_FILE="pkg/fake/zz_generated.describe_instance_types.go"
-  NO_UPDATE=''
   SUBJECT="Instance Type Test Data"
 
   go run hack/code/instancetype_testdata_gen/main.go --out-file ${GENERATED_FILE} \
     --instance-types t3.large,m5.large,m5.xlarge,p3.8xlarge,g4dn.8xlarge,c6g.large,inf1.2xlarge,inf1.6xlarge,trn1.2xlarge,m5.metal,dl1.24xlarge,m6idn.32xlarge,t4g.small,t4g.xlarge,t4g.medium
 
-  GIT_DIFF=$(git diff --stat "${GENERATED_FILE}")
-  checkForUpdates "${GIT_DIFF}" "${NO_UPDATE}" "${SUBJECT}" "${GENERATED_FILE}"
+  checkForUpdates "${SUBJECT}" "${GENERATED_FILE}"
 }
 
 checkForUpdates() {
-  GIT_DIFF=$1
-  NO_UPDATE=$2
-  SUBJECT=$3
-  GENERATED_FILE=$4
+  SUBJECT=$1
+  GENERATED_FILE=$2
+  IGNORE_PATTERN=${3:-""}
 
-  echo "Checking git diff for updates. ${GIT_DIFF}, ${NO_UPDATE}"
-  if [[ "${GIT_DIFF}" == "${NO_UPDATE}" ]]; then
-    noUpdates "${SUBJECT}"
-    git checkout "${GENERATED_FILE}"
+  if [[ -z "$IGNORE_PATTERN" ]]; then
+    GIT_DIFF=$(git diff --stat --ignore-blank-lines "${GENERATED_FILE}")
   else
-    echo "true" >/tmp/codegen-updates
+    GIT_DIFF=$(git diff --stat --ignore-blank-lines --ignore-matching-lines="${IGNORE_PATTERN}" "${GENERATED_FILE}")
+  fi
+
+  echo "Checking git diff for updates..."
+  if [[ -n "${GIT_DIFF}" ]]; then
+    echo "$GIT_DIFF"
     git add "${GENERATED_FILE}"
     if [[ $ENABLE_GIT_PUSH == true ]]; then
       gitCommitAndPush "${SUBJECT}"
     fi
+  else
+    noUpdates "${SUBJECT}"
+    git checkout "${GENERATED_FILE}"
   fi
 }
 
