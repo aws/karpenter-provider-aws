ab3d58f33745cc80ea3bee2d2e2b398b86ac11db
Jason Deal <jmdeal@amazon.com>
2024-02-06 12:23:20 -0800

    chore: Release v0.34.0 (#5614)

---
+++ website/content/en/v0.34/getting-started/getting-started-with-karpenter/scripts/step08-apply-helm-chart.sh
@@ -0,0 +1,11 @@
+# Logout of helm registry to perform an unauthenticated pull against the public ECR
+helm registry logout public.ecr.aws
+
+helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" --create-namespace \
+  --set "settings.clusterName=${CLUSTER_NAME}" \
+  --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+  --set controller.resources.requests.cpu=1 \
+  --set controller.resources.requests.memory=1Gi \
+  --set controller.resources.limits.cpu=1 \
+  --set controller.resources.limits.memory=1Gi \
+  --wait
