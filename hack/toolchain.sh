78dfefcf3f39c8239ce2130807c362f4dc6cf425
Steve Hipwell <steve.hipwell@gmail.com>
2024-02-16 04:35:55 +0000

    feat(chart): Added cosign signature (#5609)
    Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

--- hack/toolchain.sh
+++ hack/toolchain.sh
@@ -23,6 +23,7 @@ tools() {
     go install github.com/onsi/ginkgo/v2/ginkgo@latest
     go install github.com/rhysd/actionlint/cmd/actionlint@latest
     go install github.com/mattn/goveralls@latest
+    go install github.com/google/go-containerregistry/cmd/crane@latest
 
     if ! echo "$PATH" | grep -q "${GOPATH:-undefined}/bin\|$HOME/go/bin"; then
         echo "Go workspace's \"bin\" directory is not in PATH. Run 'export PATH=\"\$PATH:\${GOPATH:-\$HOME/go}/bin\"'."
