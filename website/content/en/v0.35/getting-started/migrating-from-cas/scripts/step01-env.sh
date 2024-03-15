3006a457344110703ec4a223484351fa646f0e92
njtran <njtran@amazon.com>
2024-03-15 14:32:05 -0700

    fix website

--- website/content/en/v0.35/getting-started/migrating-from-cas/scripts/step01-env.sh
+++ website/content/en/v0.35/getting-started/migrating-from-cas/scripts/step01-env.sh
@@ -4,5 +4,6 @@ OIDC_ENDPOINT="$(aws eks describe-cluster --name "${CLUSTER_NAME}" \
     --query "cluster.identity.oidc.issuer" --output text)"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' \
     --output text)
+K8S_VERSION=1.28
 ARM_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2-arm64/recommended/image_id --query Parameter.Value --output text)"
 AMD_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2/recommended/image_id --query Parameter.Value --output text)"
--- website/go.mod
+++ website/go.mod
@@ -1,6 +1,6 @@
 module github.com/aws/karpenter-provider-aws/website
 
-go 1.22
+go 1.22.0 // instead of 1.22 https://github.com/golang/go/issues/65568#issuecomment-1954876836
 
 require (
 	github.com/google/docsy v0.6.0 // indirect
