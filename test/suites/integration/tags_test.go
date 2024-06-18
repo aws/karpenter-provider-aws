93fe735124fbb97e9607cfebd939461eefe83707
Reed Schalo <schalor@amazon.com>
2024-06-18 10:31:34 -0700

    expand test

--- test/suites/integration/tags_test.go
+++ test/suites/integration/tags_test.go
@@ -101,6 +101,7 @@ var _ = Describe("Tags", func() {
 			}, time.Minute)
 
 			nodeInstance := instance.NewInstance(lo.ToPtr(env.GetInstance(node.Name)))
+			Expect(nodeInstance.Tags).To(HaveKeyWithValue(v1beta1.EKSClusterNameAnnotationKey, env.ClusterName))
 			Expect(nodeInstance.Tags).To(HaveKeyWithValue("Name", node.Name))
 			Expect(nodeInstance.Tags).To(HaveKey("karpenter.sh/nodeclaim"))
 		})
