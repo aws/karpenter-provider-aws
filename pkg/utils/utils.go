f7f0470d58aff2feffc55ff785162ecbb114f68d
Jason Deal <jmdeal@amazon.com>
2024-07-11 14:11:23 -0700

    chore: parse kubelet config annotation

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -15,13 +15,17 @@ limitations under the License.
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	v1 "github.com/aws/karpenter-provider-aws/pkg/apis/v1"
 	"github.com/samber/lo"
+	karpv1beta1 "sigs.k8s.io/karpenter/pkg/apis/v1beta1"
+	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 )
 
 var (
@@ -65,4 +69,44 @@ func PrettySlice[T any](s []T, maxItems int) string {
 		fmt.Fprint(&sb, elem)
 	}
 	return sb.String()
+}
+
+// GetKubletConfigurationWithNodePool use the most recent version of the kubelet configuration.
+// The priority of fields is listed below:
+// 1.) v1 NodePool kubelet annotation (Showing a user configured using v1beta1 NodePool at some point)
+// 2.) v1 EC2NodeClass will be used (showing a user configured using v1 EC2NodeClass)
+func GetKubletConfigurationWithNodePool(nodePool *karpv1.NodePool, nodeClass *v1.EC2NodeClass) (*v1.KubeletConfiguration, error) {
+	if annotation, ok := nodePool.Annotations[karpv1.KubeletCompatabilityAnnotationKey]; ok {
+		return parseKubeletConfiguration(annotation)
+	}
+	return nodeClass.Spec.Kubelet, nil
+}
+
+func GetKubeletConfigurationWithNodeClaim(nodeClaim *karpv1.NodeClaim, nodeClass *v1.EC2NodeClass) (*v1.KubeletConfiguration, error) {
+	if annotation, ok := nodeClaim.Annotations[karpv1.KubeletCompatabilityAnnotationKey]; ok {
+		return parseKubeletConfiguration(annotation)
+	}
+	return nodeClass.Spec.Kubelet, nil
+}
+
+func parseKubeletConfiguration(annotation string) (*v1.KubeletConfiguration, error) {
+	kubelet := &karpv1beta1.KubeletConfiguration{}
+	err := json.Unmarshal([]byte(annotation), kubelet)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.KubeletConfiguration{
+		ClusterDNS:                  kubelet.ClusterDNS,
+		MaxPods:                     kubelet.MaxPods,
+		PodsPerCore:                 kubelet.PodsPerCore,
+		SystemReserved:              kubelet.SystemReserved,
+		KubeReserved:                kubelet.KubeReserved,
+		EvictionSoft:                kubelet.EvictionSoft,
+		EvictionHard:                kubelet.EvictionHard,
+		EvictionSoftGracePeriod:     kubelet.EvictionSoftGracePeriod,
+		EvictionMaxPodGracePeriod:   kubelet.EvictionMaxPodGracePeriod,
+		ImageGCHighThresholdPercent: kubelet.ImageGCHighThresholdPercent,
+		ImageGCLowThresholdPercent:  kubelet.ImageGCLowThresholdPercent,
+		CPUCFSQuota:                 kubelet.CPUCFSQuota,
+	}, nil
 }
