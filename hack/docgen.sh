b06a47a86a76f42eebd2e3bd00ce82afd6de3535
Reed Schalo <schalor@amazon.com>
2024-09-05 08:07:45 -0700

    update naming

--- hack/docgen.sh
+++ hack/docgen.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 compatibilitymatrix() {
     # versionCount is the number of K8s versions to display in the compatibility matrix
     versionCount=7
-    go run hack/docs/versioncompatability/main.go hack/docs/compatibilitymatrix/compatibility-karpenter.yaml "$(git describe --exact-match --tags || echo "no tag")"
+    go run hack/docs/versioncompatibility/main.go hack/docs/compatibilitymatrix/compatibility-karpenter.yaml "$(git describe --exact-match --tags || echo "no tag")"
     go run hack/docs/compatibilitymatrix/main.go website/content/en/preview/upgrading/compatibility.md hack/docs/compatibilitymatrix/compatibility-karpenter.yaml $versionCount
 }
 
