a9329adb30ac71769ad931f1e543954973b09544
StableRelease <StableRelease@users.noreply.github.com>
2024-09-09 23:38:35 +0000

    Stable Release updates Release v0.37.3.

---
+++ website/content/en/v0.37/getting-started/getting-started-with-karpenter/scripts/step03-iam-cloud-formation.sh
@@ -0,0 +1,8 @@
+TEMPOUT="$(mktemp)"
+
+curl -fsSL https://raw.githubusercontent.com/aws/karpenter-provider-aws/v"${KARPENTER_VERSION}"/website/content/en/preview/getting-started/getting-started-with-karpenter/cloudformation.yaml  > "${TEMPOUT}" \
+&& aws cloudformation deploy \
+  --stack-name "Karpenter-${CLUSTER_NAME}" \
+  --template-file "${TEMPOUT}" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides "ClusterName=${CLUSTER_NAME}"
