fafcfde3141166da26a94fa141fd02b6846dd0c4
Jonathan Innis <joinnis@amazon.com>
2024-09-25 00:59:07 -0700

    Fix missed instance type cache assignment

--- pkg/test/environment.go
+++ pkg/test/environment.go
@@ -144,6 +144,7 @@ func NewEnvironment(ctx context.Context, env *coretest.Environment) *Environment
 
 		EC2Cache:                      ec2Cache,
 		KubernetesVersionCache:        kubernetesVersionCache,
+		InstanceTypeCache:             instanceTypeCache,
 		LaunchTemplateCache:           launchTemplateCache,
 		SubnetCache:                   subnetCache,
 		AvailableIPAdressCache:        availableIPAdressCache,
