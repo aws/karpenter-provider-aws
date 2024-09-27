c7b19a0914b23c888207a8fd13fda19e7590fd0f
StableRelease <StableRelease@users.noreply.github.com>
2024-09-27 23:56:19 +0000

    Stable Release updates Release v0.35.9.

--- website/content/en/v0.31/getting-started/getting-started-with-karpenter/scripts/step01-config.sh
+++
@@ -1,5 +0,0 @@
-export AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
-export CLUSTER_NAME="${USER}-karpenter-demo"
-export AWS_DEFAULT_REGION="us-west-2"
-export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-export TEMPOUT=$(mktemp)
