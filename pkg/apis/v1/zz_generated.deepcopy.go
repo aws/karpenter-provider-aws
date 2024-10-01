zeronito/karpenter-aws

RFC Implementation Supporting ODCR

pkg/apis/v1/zz_generated.deepcopy.go
@@ -150,6 +150,48 @@ func (in *BlockDeviceMapping) DeepCopy() *BlockDeviceMapping {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CapacityReservation) DeepCopyInto(out *CapacityReservation) {
+	*out = *in
+	if in.EndDate != nil {
+		in, out := &in.EndDate, &out.EndDate
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CapacityReservation.
+func (in *CapacityReservation) DeepCopy() *CapacityReservation {
+	if in == nil {
+		return nil
+	}
+	out := new(CapacityReservation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CapacityReservationSelectorTerm) DeepCopyInto(out *CapacityReservationSelectorTerm) {
+	*out = *in
+	if in.Tags != nil {
+		in, out := &in.Tags, &out.Tags
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CapacityReservationSelectorTerm.
+func (in *CapacityReservationSelectorTerm) DeepCopy() *CapacityReservationSelectorTerm {
+	if in == nil {
+		return nil
+	}
+	out := new(CapacityReservationSelectorTerm)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *EC2NodeClass) DeepCopyInto(out *EC2NodeClass) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
@@ -242,6 +284,13 @@ func (in *EC2NodeClassSpec) DeepCopyInto(out *EC2NodeClassSpec) {
 		*out = new(string)
 		**out = **in
 	}
+	if in.CapacityReservationSelectorTerms != nil {
+		in, out := &in.CapacityReservationSelectorTerms, &out.CapacityReservationSelectorTerms
+		*out = make([]CapacityReservationSelectorTerm, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.UserData != nil {
 		in, out := &in.UserData, &out.UserData
 		*out = new(string)
@@ -310,6 +359,13 @@ func (in *EC2NodeClassSpec) DeepCopy() *EC2NodeClassSpec {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *EC2NodeClassStatus) DeepCopyInto(out *EC2NodeClassStatus) {
 	*out = *in
+	if in.CapacityReservations != nil {
+		in, out := &in.CapacityReservations, &out.CapacityReservations
+		*out = make([]CapacityReservation, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.Subnets != nil {
 		in, out := &in.Subnets, &out.Subnets
 		*out = make([]Subnet, len(*in))

pkg/providers/amifamily/resolver.go
@@ -66,14 +66,15 @@ type Options struct {
 // LaunchTemplate holds the dynamically generated launch template parameters
 type LaunchTemplate struct {
 	*Options
-	UserData            bootstrap.Bootstrapper
-	BlockDeviceMappings []*v1.BlockDeviceMapping
-	MetadataOptions     *v1.MetadataOptions
-	AMIID               string
-	InstanceTypes       []*cloudprovider.InstanceType `hash:"ignore"`
-	DetailedMonitoring  bool
-	EFACount            int
-	CapacityType        string
+	UserData              bootstrap.Bootstrapper
+	BlockDeviceMappings   []*v1.BlockDeviceMapping
+	MetadataOptions       *v1.MetadataOptions
+	AMIID                 string
+	InstanceTypes         []*cloudprovider.InstanceType `hash:"ignore"`
+	DetailedMonitoring    bool
+	EFACount              int
+	CapacityType          string
+	CapacityReservationID *string
 }
 
 // AMIFamily can be implemented to override the default logic for generating dynamic launch template parameters
@@ -150,12 +151,54 @@ func (r Resolver) Resolve(nodeClass *v1.EC2NodeClass, nodeClaim *karpv1.NodeClai
 				maxPods: int(instanceType.Capacity.Pods().Value()),
 			}
 		})
+		// tvonhacht: figure out if capacity-reservation is available
 		for params, instanceTypes := range paramsToInstanceTypes {
-			resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, options)
+			if r.isCapacityReservationLaunch(nodeClaim, instanceTypes) {
+				requirements := scheduling.NewNodeSelectorRequirementsWithMinValues(nodeClaim.Spec.Requirements...)
+				for _, instanceType := range instanceTypes {
+					for _, offering := range instanceType.Offerings {
+						requirements[karpv1.CapacityTypeLabelKey] = scheduling.NewRequirement(karpv1.CapacityTypeLabelKey, corev1.NodeSelectorOpIn, karpv1.CapacityTypeOnDemand)
+						requirements[v1.LabelCapactiyReservationID] = scheduling.NewRequirement(v1.LabelCapactiyReservationID, corev1.NodeSelectorOpExists)
+						if requirements.Compatible(offering.Requirements, scheduling.AllowUndefinedWellKnownLabels) != nil {
+							continue
+						}
+
+						capacityReservationID := offering.Requirements.Get(v1.LabelCapactiyReservationID).Values()[0]
+
+						resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, &capacityReservationID, options)
+						resolvedTemplates = append(resolvedTemplates, resolved)
+					}
+				}
+				continue
+			}
+
+			resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, nil, options)
 			resolvedTemplates = append(resolvedTemplates, resolved)
 		}
 	}
 	return resolvedTemplates, nil
+}
+
+func (r Resolver) isCapacityReservationLaunch(nodeClaim *karpv1.NodeClaim, instanceTypes []*cloudprovider.InstanceType) bool {
+	requirements := scheduling.NewNodeSelectorRequirementsWithMinValues(nodeClaim.Spec.Requirements...)
+	// requirements must allow on-demand
+	if !requirements.Get(karpv1.CapacityTypeLabelKey).Has(karpv1.CapacityTypeOnDemand) {
+		return false
+	}
+
+	requirements[karpv1.CapacityTypeLabelKey] = scheduling.NewRequirement(karpv1.CapacityTypeLabelKey, corev1.NodeSelectorOpIn, karpv1.CapacityTypeOnDemand)
+	requirements[v1.LabelCapactiyReservationID] = scheduling.NewRequirement(v1.LabelCapactiyReservationID, corev1.NodeSelectorOpExists)
+	for _, instanceType := range instanceTypes {
+		for _, offering := range instanceType.Offerings {
+			if requirements.Compatible(offering.Requirements, scheduling.AllowUndefinedWellKnownLabels) == nil {
+				if offering.Requirements.Has(v1.LabelCapactiyReservationID) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
 
 func GetAMIFamily(amiFamily string, options *Options) AMIFamily {
@@ -202,7 +245,7 @@ func (r Resolver) defaultClusterDNS(opts *Options, kubeletConfig *v1.KubeletConf
 }
 
 func (r Resolver) resolveLaunchTemplate(nodeClass *v1.EC2NodeClass, nodeClaim *karpv1.NodeClaim, instanceTypes []*cloudprovider.InstanceType, capacityType string,
-	amiFamily AMIFamily, amiID string, maxPods int, efaCount int, options *Options) *LaunchTemplate {
+	amiFamily AMIFamily, amiID string, maxPods int, efaCount int, capacityReservationID *string, options *Options) *LaunchTemplate {
 	kubeletConfig := nodeClass.Spec.Kubelet
 	if kubeletConfig == nil {
 		kubeletConfig = &v1.KubeletConfiguration{}
@@ -234,13 +277,14 @@ func (r Resolver) resolveLaunchTemplate(nodeClass *v1.EC2NodeClass, nodeClaim *k
 			nodeClass.Spec.UserData,
 			options.InstanceStorePolicy,
 		),
-		BlockDeviceMappings: nodeClass.Spec.BlockDeviceMappings,
-		MetadataOptions:     nodeClass.Spec.MetadataOptions,
-		DetailedMonitoring:  aws.BoolValue(nodeClass.Spec.DetailedMonitoring),
-		AMIID:               amiID,
-		InstanceTypes:       instanceTypes,
-		EFACount:            efaCount,
-		CapacityType:        capacityType,
+		BlockDeviceMappings:   nodeClass.Spec.BlockDeviceMappings,
+		MetadataOptions:       nodeClass.Spec.MetadataOptions,
+		DetailedMonitoring:    aws.BoolValue(nodeClass.Spec.DetailedMonitoring),
+		AMIID:                 amiID,
+		InstanceTypes:         instanceTypes,
+		EFACount:              efaCount,
+		CapacityType:          capacityType,
+		CapacityReservationID: capacityReservationID,
 	}
 	if len(resolved.BlockDeviceMappings) == 0 {
 		resolved.BlockDeviceMappings = amiFamily.DefaultBlockDeviceMappings()

aws/karpenter-provider-aws

pkg/apis/v1/zz_generated.deepcopy.go
@@ -150,6 +150,47 @@ func (in *BlockDeviceMapping) DeepCopy() *BlockDeviceMapping {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CapacityReservation) DeepCopyInto(out *CapacityReservation) {
+	*out = *in
+	if in.EndTime != nil {
+		in, out := &in.EndTime, &out.EndTime
+		*out = (*in).DeepCopy()
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CapacityReservation.
+func (in *CapacityReservation) DeepCopy() *CapacityReservation {
+	if in == nil {
+		return nil
+	}
+	out := new(CapacityReservation)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CapacityReservationSelectorTerm) DeepCopyInto(out *CapacityReservationSelectorTerm) {
+	*out = *in
+	if in.Tags != nil {
+		in, out := &in.Tags, &out.Tags
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CapacityReservationSelectorTerm.
+func (in *CapacityReservationSelectorTerm) DeepCopy() *CapacityReservationSelectorTerm {
+	if in == nil {
+		return nil
+	}
+	out := new(CapacityReservationSelectorTerm)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *EC2NodeClass) DeepCopyInto(out *EC2NodeClass) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
@@ -242,6 +283,13 @@ func (in *EC2NodeClassSpec) DeepCopyInto(out *EC2NodeClassSpec) {
 		*out = new(string)
 		**out = **in
 	}
+	if in.CapacityReservationSelectorTerms != nil {
+		in, out := &in.CapacityReservationSelectorTerms, &out.CapacityReservationSelectorTerms
+		*out = make([]CapacityReservationSelectorTerm, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.UserData != nil {
 		in, out := &in.UserData, &out.UserData
 		*out = new(string)
@@ -310,6 +358,13 @@ func (in *EC2NodeClassSpec) DeepCopy() *EC2NodeClassSpec {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *EC2NodeClassStatus) DeepCopyInto(out *EC2NodeClassStatus) {
 	*out = *in
+	if in.CapacityReservations != nil {
+		in, out := &in.CapacityReservations, &out.CapacityReservations
+		*out = make([]CapacityReservation, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.Subnets != nil {
 		in, out := &in.Subnets, &out.Subnets
 		*out = make([]Subnet, len(*in))

pkg/providers/amifamily/resolver.go
@@ -66,14 +66,15 @@ type Options struct {
 // LaunchTemplate holds the dynamically generated launch template parameters
 type LaunchTemplate struct {
 	*Options
-	UserData            bootstrap.Bootstrapper
-	BlockDeviceMappings []*v1.BlockDeviceMapping
-	MetadataOptions     *v1.MetadataOptions
-	AMIID               string
-	InstanceTypes       []*cloudprovider.InstanceType `hash:"ignore"`
-	DetailedMonitoring  bool
-	EFACount            int
-	CapacityType        string
+	UserData              bootstrap.Bootstrapper
+	BlockDeviceMappings   []*v1.BlockDeviceMapping
+	MetadataOptions       *v1.MetadataOptions
+	AMIID                 string
+	InstanceTypes         []*cloudprovider.InstanceType `hash:"ignore"`
+	DetailedMonitoring    bool
+	EFACount              int
+	CapacityType          string
+	CapacityReservationID *string
 }
 
 // AMIFamily can be implemented to override the default logic for generating dynamic launch template parameters
@@ -150,12 +151,54 @@ func (r Resolver) Resolve(nodeClass *v1.EC2NodeClass, nodeClaim *karpv1.NodeClai
 				maxPods: int(instanceType.Capacity.Pods().Value()),
 			}
 		})
+		// tvonhacht: figure out if capacity-reservation is available
 		for params, instanceTypes := range paramsToInstanceTypes {
-			resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, options)
+			if r.isCapacityReservationLaunch(nodeClaim, instanceTypes) {
+				requirements := scheduling.NewNodeSelectorRequirementsWithMinValues(nodeClaim.Spec.Requirements...)
+				for _, instanceType := range instanceTypes {
+					for _, offering := range instanceType.Offerings {
+						requirements[karpv1.CapacityTypeLabelKey] = scheduling.NewRequirement(karpv1.CapacityTypeLabelKey, corev1.NodeSelectorOpIn, karpv1.CapacityTypeOnDemand)
+						requirements[v1.LabelCapactiyReservationID] = scheduling.NewRequirement(v1.LabelCapactiyReservationID, corev1.NodeSelectorOpExists)
+						if requirements.Compatible(offering.Requirements, scheduling.AllowUndefinedWellKnownLabels) != nil {
+							continue
+						}
+
+						for _, capacityReservationID := range offering.Requirements.Get(v1.LabelCapactiyReservationID).Values() {
+							resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, &capacityReservationID, options)
+							resolvedTemplates = append(resolvedTemplates, resolved)
+						}
+					}
+				}
+				continue
+			}
+
+			resolved := r.resolveLaunchTemplate(nodeClass, nodeClaim, instanceTypes, capacityType, amiFamily, amiID, params.maxPods, params.efaCount, nil, options)
 			resolvedTemplates = append(resolvedTemplates, resolved)
 		}
 	}
 	return resolvedTemplates, nil
+}
+
+func (r Resolver) isCapacityReservationLaunch(nodeClaim *karpv1.NodeClaim, instanceTypes []*cloudprovider.InstanceType) bool {
+	requirements := scheduling.NewNodeSelectorRequirementsWithMinValues(nodeClaim.Spec.Requirements...)
+	// requirements must allow reserved
+	if !requirements.Get(karpv1.CapacityTypeLabelKey).Has(karpv1.CapacityTypeReserved) {
+		return false
+	}
+
+	requirements[karpv1.CapacityTypeLabelKey] = scheduling.NewRequirement(karpv1.CapacityTypeLabelKey, corev1.NodeSelectorOpIn, karpv1.CapacityTypeReserved)
+	requirements[v1.LabelCapactiyReservationID] = scheduling.NewRequirement(v1.LabelCapactiyReservationID, corev1.NodeSelectorOpExists)
+	for _, instanceType := range instanceTypes {
+		for _, offering := range instanceType.Offerings {
+			if requirements.Compatible(offering.Requirements, scheduling.AllowUndefinedWellKnownLabels) == nil {
+				if offering.Requirements.Has(v1.LabelCapactiyReservationID) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
 
 func GetAMIFamily(amiFamily string, options *Options) AMIFamily {
@@ -202,7 +245,7 @@ func (r Resolver) defaultClusterDNS(opts *Options, kubeletConfig *v1.KubeletConf
 }
 
 func (r Resolver) resolveLaunchTemplate(nodeClass *v1.EC2NodeClass, nodeClaim *karpv1.NodeClaim, instanceTypes []*cloudprovider.InstanceType, capacityType string,
-	amiFamily AMIFamily, amiID string, maxPods int, efaCount int, options *Options) *LaunchTemplate {
+	amiFamily AMIFamily, amiID string, maxPods int, efaCount int, capacityReservationID *string, options *Options) *LaunchTemplate {
 	kubeletConfig := nodeClass.Spec.Kubelet
 	if kubeletConfig == nil {
 		kubeletConfig = &v1.KubeletConfiguration{}
@@ -234,13 +277,14 @@ func (r Resolver) resolveLaunchTemplate(nodeClass *v1.EC2NodeClass, nodeClaim *k
 			nodeClass.Spec.UserData,
 			options.InstanceStorePolicy,
 		),
-		BlockDeviceMappings: nodeClass.Spec.BlockDeviceMappings,
-		MetadataOptions:     nodeClass.Spec.MetadataOptions,
-		DetailedMonitoring:  aws.BoolValue(nodeClass.Spec.DetailedMonitoring),
-		AMIID:               amiID,
-		InstanceTypes:       instanceTypes,
-		EFACount:            efaCount,
-		CapacityType:        capacityType,
+		BlockDeviceMappings:   nodeClass.Spec.BlockDeviceMappings,
+		MetadataOptions:       nodeClass.Spec.MetadataOptions,
+		DetailedMonitoring:    aws.BoolValue(nodeClass.Spec.DetailedMonitoring),
+		AMIID:                 amiID,
+		InstanceTypes:         instanceTypes,
+		EFACount:              efaCount,
+		CapacityType:          capacityType,
+		CapacityReservationID: capacityReservationID,
 	}
 	if len(resolved.BlockDeviceMappings) == 0 {
 		resolved.BlockDeviceMappings = amiFamily.DefaultBlockDeviceMappings()