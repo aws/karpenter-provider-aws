6a0017d4fd5fecb61d41b5a6dab87084ec18b252
jukie <10012479+Jukie@users.noreply.github.com>
2024-10-03 15:19:25 -0600

    Simplify inititialization
    Signed-off-by: jukie <10012479+Jukie@users.noreply.github.com>

--- pkg/controllers/providers/instancetype/vmcapacitycache/controller.go
+++ pkg/controllers/providers/instancetype/vmcapacitycache/controller.go
@@ -19,21 +19,17 @@ import (
 	"fmt"
 	"github.com/aws/karpenter-provider-aws/pkg/providers/instancetype"
 	"github.com/awslabs/operatorpkg/reasonable"
+	"github.com/awslabs/operatorpkg/singleton"
 	"go.uber.org/multierr"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/util/workqueue"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 	"sigs.k8s.io/karpenter/pkg/operator/injection"
 )
@@ -41,6 +37,7 @@ import (
 type Controller struct {
 	kubeClient           client.Client
 	instancetypeProvider *instancetype.DefaultProvider
+	initialized          bool
 }
 
 func NewController(kubeClient client.Client, instancetypeProvider *instancetype.DefaultProvider) *Controller {
@@ -50,11 +47,9 @@ func NewController(kubeClient client.Client, instancetypeProvider *instancetype.
 	}
 }
 
-const initNodeName = "initialize-capacity-cache-nodes"
-
 func (c *Controller) Reconcile(ctx context.Context, r reconcile.Request) (reconcile.Result, error) {
 	ctx = injection.WithControllerName(ctx, "instancetypes.vmcapacitycache")
-	if r.Name == initNodeName {
+	if !c.initialized {
 		return c.reconcileAllNodes(ctx)
 	}
 	if err := c.instancetypeProvider.UpdateVMCapacityCache(ctx, c.kubeClient, r.Name); err != nil {
@@ -72,20 +67,21 @@ func (c *Controller) reconcileAllNodes(ctx context.Context) (reconcile.Result, e
 	}
 	errs := make([]error, len(nodeList.Items))
 	for i, node := range nodeList.Items {
-		if _, err := c.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: node.Name}}); err != nil {
+		if err := c.instancetypeProvider.UpdateVMCapacityCache(ctx, c.kubeClient, node.Name); err != nil {
 			errs[i] = err
 		}
 	}
 	if err := multierr.Combine(errs...); err != nil {
 		return reconcile.Result{}, fmt.Errorf("updating vm capacity cache, %w", err)
 	}
+	c.initialized = true
 	return reconcile.Result{}, nil
 }
 
 func (c *Controller) Register(_ context.Context, m manager.Manager) error {
 	return controllerruntime.NewControllerManagedBy(m).
 		Named("instancetypes.vmcapacitycache").
-		WatchesRawSource(initialLoad()).
+		WatchesRawSource(singleton.Source()).
 		For(&corev1.Node{}).
 		WithEventFilter(predicate.Funcs{
 			// Only trigger reconciliation once a node becomes registered
@@ -106,14 +102,4 @@ func (c *Controller) Register(_ context.Context, m manager.Manager) error {
 			MaxConcurrentReconciles: 1,
 		}).
 		Complete(c)
-}
-
-func initialLoad() source.Source {
-	events := make(chan event.GenericEvent, 1)
-	events <- event.GenericEvent{Object: &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: initNodeName}}}
-	return source.Channel(events, &handler.Funcs{
-		GenericFunc: func(_ context.Context, evt event.GenericEvent, queue workqueue.TypedRateLimitingInterface[reconcile.Request]) {
-			queue.Add(reconcile.Request{NamespacedName: types.NamespacedName{Name: evt.Object.GetName()}})
-		},
-	})
 }
