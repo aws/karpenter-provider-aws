c37fc55a8bc6c922dbf0ecb367821a8c07555fe1
Reed Schalo <schalor@amazon.com>
2024-10-18 14:51:21 -0700

    update comment

--- hack/mutation/conversion_webhook_injection.sh
+++ hack/mutation/conversion_webhook_injection.sh
@@ -6,7 +6,7 @@ yq eval '.spec.conversion = {"strategy": "Webhook", "webhook": {"conversionRevie
 yq eval '.spec.conversion = {"strategy": "Webhook", "webhook": {"conversionReviewVersions": ["v1beta1", "v1"], "clientConfig": {"service": {"name": "karpenter", "namespace": "kube-system", "port": 8443}}}}' -i pkg/apis/crds/karpenter.sh_nodepools.yaml
 
 # Update to the karpenter-crd charts
-# Remove the copied over versions and conversion stanzas from CRD spec
+# Remove the copied conversion stanzas from CRD specs
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.k8s.aws_ec2nodeclasses.yaml
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.sh_nodeclaims.yaml
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
