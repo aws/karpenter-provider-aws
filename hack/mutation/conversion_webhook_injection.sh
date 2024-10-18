5fbdb10675c771b6a446fcdb6e3f4730f2cc46e6
Reed Schalo <schalor@amazon.com>
2024-10-18 09:15:11 -0700

    template versions stanza

--- hack/mutation/conversion_webhook_injection.sh
+++ hack/mutation/conversion_webhook_injection.sh
@@ -6,54 +6,15 @@ yq eval '.spec.conversion = {"strategy": "Webhook", "webhook": {"conversionRevie
 yq eval '.spec.conversion = {"strategy": "Webhook", "webhook": {"conversionReviewVersions": ["v1beta1", "v1"], "clientConfig": {"service": {"name": "karpenter", "namespace": "kube-system", "port": 8443}}}}' -i pkg/apis/crds/karpenter.sh_nodepools.yaml
 
 # Update to the karpenter-crd charts
-
-# Remove the copied over conversion stanzas from CRD spec
+# Remove the copied over versions and conversion stanzas from CRD spec
+yq eval 'del(.spec.versions)' -i charts/karpenter-crd/templates/karpenter.k8s.aws_ec2nodeclasses.yaml
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.k8s.aws_ec2nodeclasses.yaml
+yq eval 'del(.spec.versions)' -i charts/karpenter-crd/templates/karpenter.sh_nodeclaims.yaml
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.sh_nodeclaims.yaml
+yq eval 'del(.spec.versions)' -i charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
 yq eval 'del(.spec.conversion)' -i charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
 
-# Add the conversion stanza template to the CRD spec to enable conversion via webhook
-echo "{{- if .Values.webhook.enabled }} 
-  conversion:
-    strategy: Webhook
-    webhook:
-      conversionReviewVersions:
-        - v1beta1
-        - v1
-      clientConfig:
-        service:
-          name: {{ .Values.webhook.serviceName }}
-          namespace: {{ .Values.webhook.serviceNamespace | default .Release.Namespace }}
-          port: {{ .Values.webhook.port }}
-{{- end }}
-" >>  charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
-
-echo "{{- if .Values.webhook.enabled }} 
-  conversion:
-    strategy: Webhook
-    webhook:
-      conversionReviewVersions:
-        - v1beta1
-        - v1
-      clientConfig:
-        service:
-          name: {{ .Values.webhook.serviceName }}
-          namespace: {{ .Values.webhook.serviceNamespace | default .Release.Namespace }}
-          port: {{ .Values.webhook.port }}
-{{- end }}
-" >>  charts/karpenter-crd/templates/karpenter.sh_nodeclaims.yaml
-
-echo "{{- if .Values.webhook.enabled }} 
-  conversion:
-    strategy: Webhook
-    webhook:
-      conversionReviewVersions:
-        - v1beta1
-        - v1
-      clientConfig:
-        service:
-          name: {{ .Values.webhook.serviceName }}
-          namespace: {{ .Values.webhook.serviceNamespace | default .Release.Namespace }}
-          port: {{ .Values.webhook.port }}
-{{- end }}
-" >>  charts/karpenter-crd/templates/karpenter.k8s.aws_ec2nodeclasses.yaml
+# Template the v1 version and the conversion strategy of the spec 
+hack/mutation/ec2nodeclasses.sh
+hack/mutation/nodepools.sh
+hack/mutation/nodeclaims.sh
