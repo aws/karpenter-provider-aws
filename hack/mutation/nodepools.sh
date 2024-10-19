39718d78a4903fb91a797574c0ace6e5093127ce
Reed Schalo <reed0schalo@gmail.com>
2024-10-18 17:03:22 -0700

    fix: Template v1 and Conversion CRD stanzas v0.37.x (#7237)

---
+++ hack/mutation/nodepools.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+VERSION_START="$(cat charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml | yq '.spec.versions.[0] | line')"
+VERSION_END="$(cat charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml | yq '.spec.versions.[1] | line')"
+VERSION_END=$(($VERSION_END+1))
+TEMP=$(mktemp)
+
+cat charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml | awk -v n=$VERSION_START 'NR==n {sub(/$/,"\n{{- if .Values.webhook.enabled }}")} 1' \
+| awk -v n=$VERSION_END 'NR==n {sub(/$/,"\n{{- end }}")} 1' > $TEMP
+
+cat $TEMP > charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
+
+echo "{{- if .Values.webhook.enabled }} 
+  conversion:
+    strategy: Webhook
+    webhook:
+      conversionReviewVersions:
+        - v1beta1
+        - v1
+      clientConfig:
+        service:
+          name: {{ .Values.webhook.serviceName }}
+          namespace: {{ .Values.webhook.serviceNamespace | default .Release.Namespace }}
+          port: {{ .Values.webhook.port }}
+{{- end }}
+" >>  charts/karpenter-crd/templates/karpenter.sh_nodepools.yaml
