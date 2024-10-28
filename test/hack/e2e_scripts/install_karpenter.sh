c1fd391538c710475a17914c342f3bf91a06460e
Reed Schalo <schalor@amazon.com>
2024-10-28 11:13:53 -0700

    use install script in gh action

--- test/hack/e2e_scripts/install_karpenter.sh
+++ test/hack/e2e_scripts/install_karpenter.sh
@@ -4,33 +4,28 @@ aws eks update-kubeconfig --name "$CLUSTER_NAME"
 if (( "$WEBHOOKS_ENABLED" == false )); then
 helm upgrade --install karpenter-crd oci://$ECR_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/karpenter/snapshot/karpenter-crd \
   --namespace kube-system \
-  --version "0-$(git rev-parse HEAD)" \
+  --version "v0-$(git rev-parse HEAD)" \
   --set webhook.enabled=${WEBHOOKS_ENABLED} \
   --wait
 fi
 
 CHART="oci://$ECR_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/karpenter/snapshot/karpenter"
 ADDITIONAL_FLAGS=""
-if (( "$PRIVATE_CLUSTER" == 'true' )); then
-  CHART="oci://$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/karpenter/snapshot/karpenter"
-  ADDITIONAL_FLAGS="--set .Values.controller.image.repository=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/karpenter/snapshot/controller --set .Values.controller.image.digest=\"\""
-fi
+
 
 # Remove service account annotation when dropping support for 1.23
 helm upgrade --install karpenter "${CHART}" \
   -n kube-system \
-  --version "0-$(git rev-parse HEAD)" \
+  --version "v0-$(git rev-parse HEAD)" \
   --set logLevel=debug \
   --set webhook.enabled=${WEBHOOKS_ENABLED} \
-  --set settings.isolatedVPC=${PRIVATE_CLUSTER} \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="arn:aws:iam::$ACCOUNT_ID:role/karpenter-irsa-$CLUSTER_NAME" \
-  $ADDITIONAL_FLAGS \
   --set settings.clusterName="$CLUSTER_NAME" \
   --set settings.interruptionQueue="$CLUSTER_NAME" \
   --set settings.featureGates.spotToSpotConsolidation=true \
-  --set controller.resources.requests.cpu=5 \
+  --set controller.resources.requests.cpu=3 \
   --set controller.resources.requests.memory=3Gi \
-  --set controller.resources.limits.cpu=5 \
+  --set controller.resources.limits.cpu=3 \
   --set controller.resources.limits.memory=3Gi \
   --set serviceMonitor.enabled=true \
   --set serviceMonitor.additionalLabels.scrape=enabled \
