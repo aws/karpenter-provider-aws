bac89fe90d5d8ce3ce0eb601310ec3aa81982123
Reed Schalo <schalor@amazon.com>
2024-10-28 11:05:16 -0700

    use install script in gh action

--- test/hack/e2e_scripts/install_karpenter.sh
+++ test/hack/e2e_scripts/install_karpenter.sh
@@ -11,10 +11,6 @@ fi
 
 CHART="oci://$ECR_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/karpenter/snapshot/karpenter"
 ADDITIONAL_FLAGS=""
-if (( "$PRIVATE_CLUSTER" == 'true' )); then
-  CHART="oci://$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/karpenter/snapshot/karpenter"
-  ADDITIONAL_FLAGS="--set .Values.controller.image.repository=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/karpenter/snapshot/controller --set .Values.controller.image.digest=\"\""
-fi
 
 # Remove service account annotation when dropping support for 1.23
 helm upgrade --install karpenter "${CHART}" \
@@ -22,15 +18,13 @@ helm upgrade --install karpenter "${CHART}" \
   --version "0-$(git rev-parse HEAD)" \
   --set logLevel=debug \
   --set webhook.enabled=${WEBHOOKS_ENABLED} \
-  --set settings.isolatedVPC=${PRIVATE_CLUSTER} \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="arn:aws:iam::$ACCOUNT_ID:role/karpenter-irsa-$CLUSTER_NAME" \
-  $ADDITIONAL_FLAGS \
   --set settings.clusterName="$CLUSTER_NAME" \
   --set settings.interruptionQueue="$CLUSTER_NAME" \
   --set settings.featureGates.spotToSpotConsolidation=true \
-  --set controller.resources.requests.cpu=5 \
+  --set controller.resources.requests.cpu=3 \
   --set controller.resources.requests.memory=3Gi \
-  --set controller.resources.limits.cpu=5 \
+  --set controller.resources.limits.cpu=3 \
   --set controller.resources.limits.memory=3Gi \
   --set serviceMonitor.enabled=true \
   --set serviceMonitor.additionalLabels.scrape=enabled \
