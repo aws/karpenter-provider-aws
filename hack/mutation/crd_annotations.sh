cd0968a8524c36c9993a4ca63022ae2a2caf35c3
Victor @ Keltio <100953003+victor-keltio@users.noreply.github.com>
2024-12-10 09:47:40 +0100

    fix: `sponge` not found

--- hack/mutation/crd_annotations.sh
+++ hack/mutation/crd_annotations.sh
@@ -5,5 +5,5 @@
 CRDS="charts/karpenter-crd/templates/*.yaml"
 for CRD in $CRDS
 do
-  awk '{print} /  annotations:/ && !n {print "    {{- with .Values.additionalAnnotations }}\n      {{- toYaml . | nindent 4 }}\n    {{- end }}"; n++}' "$CRD" | sponge "$CRD"
+  awk '{print} /  annotations:/ && !n {print "    {{- with .Values.additionalAnnotations }}\n      {{- toYaml . | nindent 4 }}\n    {{- end }}"; n++}' "$CRD" > "$CRD.new" && mv "$CRD.new" "$CRD"
 done
