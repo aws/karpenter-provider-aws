5cb81533429eaefc1a363a231a8fc7b59a30e40e
edibble21 <85638465+edibble21@users.noreply.github.com>
2025-01-27 13:45:12 -0800

    addressed feedback

--- test/suites/ami/suite_test.go
+++ test/suites/ami/suite_test.go
@@ -88,7 +88,7 @@ var _ = Describe("AMI", func() {
 
 		env.ExpectInstance(pod.Spec.NodeName).To(HaveField("ImageId", HaveValue(Equal(customAMI))))
 	})
-	FIt("should use the most recent AMI when discovering multiple", func() {
+	It("should use the most recent AMI when discovering multiple", func() {
 		// choose an old static image that will definitely have an older creation date
 		oldCustomAMI := env.GetAMIBySSMPath(fmt.Sprintf("/aws/service/eks/optimized-ami/%[1]s/amazon-linux-2023/x86_64/standard/amazon-eks-node-al2023-x86_64-standard-%[1]s-v20241213/image_id", env.K8sVersionWithOffset(1)))
 		nodeClass.Spec.AMIFamily = lo.ToPtr(v1.AMIFamilyAL2023)
