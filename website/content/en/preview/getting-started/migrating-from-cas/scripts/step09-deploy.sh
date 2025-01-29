514b81ddbe1700bb1f80ecea1cfb81221bcd15c0
edibble21 <85638465+edibble21@users.noreply.github.com>
2025-01-29 14:47:24 -0800

    fix scripts

--- website/content/en/preview/getting-started/migrating-from-cas/scripts/step09-deploy.sh
+++ website/content/en/preview/getting-started/migrating-from-cas/scripts/step09-deploy.sh
@@ -1,8 +1,8 @@
 kubectl create namespace "${KARPENTER_NAMESPACE}" || true
 kubectl create -f \
-    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodepools.yaml"
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/main/pkg/apis/crds/karpenter.sh_nodepools.yaml"
 kubectl create -f \
-    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml"
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/main/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml"
 kubectl create -f \
-    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/v${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodeclaims.yaml"
+    "https://raw.githubusercontent.com/aws/karpenter-provider-aws/main/pkg/apis/crds/karpenter.sh_nodeclaims.yaml"
 kubectl apply -f karpenter.yaml
