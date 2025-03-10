0d0ce2bcca8b97193dc8dc0d625f7301c2ab697c
edibble21 <85638465+edibble21@users.noreply.github.com>
2025-03-10 12:58:40 -0700

    catch deleted instance profile or node role

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -67,11 +67,13 @@ func NewControllers(
 	cfg aws.Config,
 	clk clock.Clock,
 	ec2api sdk.EC2API,
+	iamapi sdk.IAMAPI,
 	kubeClient client.Client,
 	recorder events.Recorder,
 	unavailableOfferings *awscache.UnavailableOfferings,
 	ssmCache *cache.Cache,
 	validationCache *cache.Cache,
+	instanceProfileCache *cache.Cache,
 	cloudProvider cloudprovider.CloudProvider,
 	subnetProvider subnet.Provider,
 	securityGroupProvider securitygroup.Provider,
@@ -86,7 +88,7 @@ func NewControllers(
 ) []controller.Controller {
 	controllers := []controller.Controller{
 		nodeclasshash.NewController(kubeClient),
-		nodeclass.NewController(ctx, clk, kubeClient, recorder, subnetProvider, securityGroupProvider, amiProvider, instanceProfileProvider, launchTemplateProvider, capacityReservationProvider, ec2api, validationCache),
+		nodeclass.NewController(ctx, clk, kubeClient, recorder, subnetProvider, securityGroupProvider, amiProvider, instanceProfileProvider, launchTemplateProvider, capacityReservationProvider, ec2api, iamapi, validationCache, instanceProfileCache),
 		nodeclaimgarbagecollection.NewController(kubeClient, cloudProvider),
 		nodeclaimtagging.NewController(kubeClient, cloudProvider, instanceProvider),
 		controllerspricing.NewController(pricingProvider),
