f8cd2b04e268ec967face7c87248a5bb0ee7654f
Derek Frank <dfrankmn@protonmail.com>
2025-03-26 16:31:14 -0700

    fix: karpenter no longer panics if SQS queue is unavailable

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -39,8 +39,6 @@ import (
 	"github.com/aws/karpenter-provider-aws/pkg/providers/launchtemplate"
 	"github.com/aws/karpenter-provider-aws/pkg/providers/version"
 
-	servicesqs "github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/samber/lo"
 	"k8s.io/utils/clock"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -101,9 +99,8 @@ func NewControllers(
 		metrics.NewController(kubeClient, cloudProvider),
 	}
 	if options.FromContext(ctx).InterruptionQueue != "" {
-		sqsapi := servicesqs.NewFromConfig(cfg)
-		out := lo.Must(sqsapi.GetQueueUrl(ctx, &servicesqs.GetQueueUrlInput{QueueName: lo.ToPtr(options.FromContext(ctx).InterruptionQueue)}))
-		controllers = append(controllers, interruption.NewController(kubeClient, cloudProvider, clk, recorder, lo.Must(sqs.NewDefaultProvider(sqsapi, lo.FromPtr(out.QueueUrl))), unavailableOfferings))
+		prov, _ := sqs.NewSQSProvider(ctx, cfg)
+		controllers = append(controllers, interruption.NewController(kubeClient, cloudProvider, clk, recorder, prov, cfg, unavailableOfferings))
 	}
 	return controllers
 }
