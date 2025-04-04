c1c253fbdeb22722315f2c456c2a9d4d0a858a01
artem-nefedov <requiem1337@gmail.com>
2025-04-04 17:41:59 +0200

    docs: Change node role policy ReadOnly -> PullOnly (support ECR pull-through cache) (#7963)

--- website/content/en/preview/getting-started/migrating-from-cas/scripts/step03-node-policies.sh
+++ website/content/en/preview/getting-started/migrating-from-cas/scripts/step03-node-policies.sh
@@ -5,7 +5,7 @@ aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
     --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEKS_CNI_Policy"
 
 aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
-    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
 
 aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
     --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonSSMManagedInstanceCore"
