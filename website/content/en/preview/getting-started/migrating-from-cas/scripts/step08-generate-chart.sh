6076fc6c3928d5f86cc4bcea8ff50a13f6c2d565
Jonathan Innis <joinnis@amazon.com>
2025-04-10 23:33:09 -0700

    Add preferencePolicy to helm settings

--- website/content/en/preview/getting-started/migrating-from-cas/scripts/step08-generate-chart.sh
+++ website/content/en/preview/getting-started/migrating-from-cas/scripts/step08-generate-chart.sh
@@ -1,6 +1,7 @@
 helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" \
     --set "settings.clusterName=${CLUSTER_NAME}" \
     --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+    --set "settings.preferencePolicy=Ignore" \
     --set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT_ID}:role/KarpenterControllerRole-${CLUSTER_NAME}" \
     --set controller.resources.requests.cpu=1 \
     --set controller.resources.requests.memory=1Gi \
