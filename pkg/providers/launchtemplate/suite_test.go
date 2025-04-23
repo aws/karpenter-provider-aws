c0ab178fffdc052be726e0033f1d172caf6c8d7f
Jason Deal <jmdeal@amazon.com>
2025-04-23 13:37:26 -0700

    ec2nodeclass status updates

--- pkg/providers/launchtemplate/suite_test.go
+++ pkg/providers/launchtemplate/suite_test.go
@@ -2347,7 +2347,7 @@ essential = true
 			CapacityReservations: crs,
 		})
 		for _, cr := range crs {
-			nodeClass.Status.CapacityReservations = append(nodeClass.Status.CapacityReservations, lo.Must(nodeclass.CapacityReservationFromEC2(&cr)))
+			nodeClass.Status.CapacityReservations = append(nodeClass.Status.CapacityReservations, lo.Must(v1.CapacityReservationFromEC2(fakeClock, &cr)))
 			awsEnv.CapacityReservationProvider.SetAvailableInstanceCount(*cr.CapacityReservationId, int(*cr.AvailableInstanceCount))
 		}
 
