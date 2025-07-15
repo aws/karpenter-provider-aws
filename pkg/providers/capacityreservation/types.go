948f27ebb832b9aa44f68e7ec3f38fec07818a6c
Jonathan Innis <jonathan.innis.ji@gmail.com>
2025-07-15 09:27:43 -0700

    perf: Ensure we are using pagination on all AWS Describe APIs (#8230)

--- pkg/providers/capacityreservation/types.go
+++ pkg/providers/capacityreservation/types.go
@@ -63,6 +63,7 @@ func (q *Query) DescribeCapacityReservationsInput() *ec2.DescribeCapacityReserva
 		Values: []string{string(ec2types.CapacityReservationStateActive)},
 	}}
 	if len(q.ID) != 0 {
+		// MaxResults can't be used when listing a capacity reservation by id
 		return &ec2.DescribeCapacityReservationsInput{
 			Filters:                filters,
 			CapacityReservationIds: []string{q.ID},
@@ -90,6 +91,8 @@ func (q *Query) DescribeCapacityReservationsInput() *ec2.DescribeCapacityReserva
 	}
 	return &ec2.DescribeCapacityReservationsInput{
 		Filters: filters,
+		// MaxResults for DescribeCapacityReservations is capped at 1000
+		MaxResults: lo.ToPtr[int32](1000),
 	}
 }
 
