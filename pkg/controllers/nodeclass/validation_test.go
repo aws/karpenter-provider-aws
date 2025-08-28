eadc8343011db553f4b8e4e796adab3e1ba3a58f
ryan-mist <ryanmist@amazon.com>
2025-08-28 16:48:47 -0700

    move testing around

--- pkg/controllers/nodeclass/validation_test.go
+++ pkg/controllers/nodeclass/validation_test.go
@@ -23,14 +23,16 @@ import (
 	"github.com/awslabs/operatorpkg/status"
 	"github.com/samber/lo"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/eks"
+	ekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
 	"github.com/aws/smithy-go"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/record"
 	karpv1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 	"sigs.k8s.io/karpenter/pkg/events"
-	coreoptions "sigs.k8s.io/karpenter/pkg/operator/options"
 	coretest "sigs.k8s.io/karpenter/pkg/test"
 
 	v1 "github.com/aws/karpenter-provider-aws/pkg/apis/v1"
@@ -89,7 +91,7 @@ var _ = Describe("NodeClass Validation Status Controller", func() {
 			Entry(v1.ConditionTypeSubnetsReady, v1.ConditionTypeSubnetsReady),
 		)
 	})
-	Context("NodeClass Status Conditions", func() {
+	Context("CIDR Resolution Precondion", func() {
 		BeforeEach(func() {
 			nodeClass = test.EC2NodeClass(v1.EC2NodeClass{
 				Spec: v1.EC2NodeClassSpec{
@@ -111,32 +113,59 @@ var _ = Describe("NodeClass Validation Status Controller", func() {
 					},
 				},
 			})
+			// Cluster CIDR will only be resolved once per lifetime of the launch template provider, reset to nil between tests
+			awsEnv.LaunchTemplateProvider.ClusterCIDR.Store(nil)
+
+			// Mock EC2 Describe Launch Template API calls to ensure CreateLaunchTemplate validation passes
+			awsEnv.EC2API.DescribeLaunchTemplatesOutput.Set(&ec2.DescribeLaunchTemplatesOutput{
+				LaunchTemplates: []ec2types.LaunchTemplate{
+					{
+						LaunchTemplateName: aws.String("test-lt"),
+						LaunchTemplateId:   aws.String("lt-12345"),
+					},
+				},
+			})
 		})
 		DescribeTable(
-			"should update status condition on nodeClass as Ready",
-			func(reservedCapacity bool) {
-				coreoptions.FromContext(ctx).FeatureGates.ReservedCapacity = reservedCapacity
+			"shouldn't resolve cluster CIDR for non-AL2023 NodeClasses",
+			func(family string, terms []v1.AMISelectorTerm) {
+				nodeClass.Spec.AMIFamily = lo.ToPtr(family)
+				nodeClass.Spec.AMISelectorTerms = terms
 				ExpectApplied(ctx, env.Client, nodeClass)
 				ExpectObjectReconciled(ctx, env.Client, controller, nodeClass)
-				nodeClass = ExpectExists(ctx, env.Client, nodeClass)
-				Expect(nodeClass.Status.Conditions).To(HaveLen(lo.Ternary(reservedCapacity, 7, 6)))
-				Expect(nodeClass.StatusConditions().Get(status.ConditionReady).IsTrue()).To(BeTrue())
+				Expect(awsEnv.LaunchTemplateProvider.ClusterCIDR.Load()).To(BeNil())
 			},
-			Entry("when reserved capacity feature flag is enabled", true),
-			Entry("when reserved capacity feature flag is disabled", false),
+			Entry(v1.AMIFamilyAL2, v1.AMIFamilyAL2, []v1.AMISelectorTerm{{Alias: "al2@latest"}}),
+			Entry(v1.AMIFamilyBottlerocket, v1.AMIFamilyBottlerocket, []v1.AMISelectorTerm{{Alias: "bottlerocket@latest"}}),
+			Entry(v1.AMIFamilyWindows2019, v1.AMIFamilyWindows2019, []v1.AMISelectorTerm{{Alias: "windows2019@latest"}}),
+			Entry(v1.AMIFamilyWindows2022, v1.AMIFamilyWindows2022, []v1.AMISelectorTerm{{Alias: "windows2022@latest"}}),
+			Entry(v1.AMIFamilyCustom, v1.AMIFamilyCustom, []v1.AMISelectorTerm{{ID: "ami-12345"}}),
 		)
-		It("should update status condition as Not Ready", func() {
-			nodeClass.Spec.SecurityGroupSelectorTerms = []v1.SecurityGroupSelectorTerm{
-				{
-					Tags: map[string]string{"foo": "invalid"},
-				},
-			}
+		It("should resolve cluster CIDR for IPv4 clusters", func() {
+			nodeClass.Spec.AMIFamily = lo.ToPtr(v1.AMIFamilyAL2023)
+			nodeClass.Spec.AMISelectorTerms = []v1.AMISelectorTerm{{Alias: "al2023@latest"}}
 			ExpectApplied(ctx, env.Client, nodeClass)
 			ExpectObjectReconciled(ctx, env.Client, controller, nodeClass)
+			Expect(lo.FromPtr(awsEnv.LaunchTemplateProvider.ClusterCIDR.Load())).To(Equal("10.100.0.0/16"))
 			nodeClass = ExpectExists(ctx, env.Client, nodeClass)
-
-			Expect(nodeClass.StatusConditions().Get(status.ConditionReady).IsFalse()).To(BeTrue())
-			Expect(nodeClass.StatusConditions().Get(status.ConditionReady).Message).To(Equal("ValidationSucceeded=False, SecurityGroupsReady=False"))
+			Expect(nodeClass.StatusConditions().IsTrue(v1.ConditionTypeValidationSucceeded)).To(BeTrue())
+		})
+		It("should resolve cluster CIDR for IPv6 clusters", func() {
+			awsEnv.EKSAPI.DescribeClusterBehavior.Output.Set(&eks.DescribeClusterOutput{
+				Cluster: &ekstypes.Cluster{
+					KubernetesNetworkConfig: &ekstypes.KubernetesNetworkConfigResponse{
+						ServiceIpv6Cidr: lo.ToPtr("2001:db8::/64"),
+					},
+					Version: lo.ToPtr("1.30"),
+				},
+			})
+			nodeClass.Spec.AMIFamily = lo.ToPtr(v1.AMIFamilyAL2023)
+			nodeClass.Spec.AMISelectorTerms = []v1.AMISelectorTerm{{Alias: "al2023@latest"}}
+			ExpectApplied(ctx, env.Client, nodeClass)
+			ExpectObjectReconciled(ctx, env.Client, controller, nodeClass)
+			Expect(lo.FromPtr(awsEnv.LaunchTemplateProvider.ClusterCIDR.Load())).To(Equal("2001:db8::/64"))
+			nodeClass = ExpectExists(ctx, env.Client, nodeClass)
+			Expect(nodeClass.StatusConditions().IsTrue(v1.ConditionTypeValidationSucceeded)).To(BeTrue())
 		})
 	})
 	Context("Tag Validation", func() {
