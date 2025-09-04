4c3df5958f1de3aa69a7e60bd86c4dc3c6deb435
ryan-mist <ryanmist@amazon.com>
2025-09-03 21:15:39 -0700

    Revert "testing workflow"
    This reverts commit 2cd3f273581849e3d8f65f1fa858d29327b3be0b.

--- hack/release/release.sh
+++ hack/release/release.sh
@@ -5,18 +5,16 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # shellcheck source=hack/release/common.sh
 source "${SCRIPT_DIR}/common.sh"
 
-# git_tag="$(git describe --exact-match --tags || echo "no tag")"
-# if [[ "${git_tag}" == "no tag" ]]; then
-#   echo "Failed to release: commit is untagged"
-#   exit 1
-# fi
-# commit_sha="$(git rev-parse HEAD)"
+git_tag="$(git describe --exact-match --tags || echo "no tag")"
+if [[ "${git_tag}" == "no tag" ]]; then
+  echo "Failed to release: commit is untagged"
+  exit 1
+fi
+commit_sha="$(git rev-parse HEAD)"
 
-# # Don't release with a dirty commit!
-# if [[ "$(git status --porcelain)" != "" ]]; then
-#   exit 1
-# fi
+# Don't release with a dirty commit!
+if [[ "$(git status --porcelain)" != "" ]]; then
+  exit 1
+fi
 
-git_tag="v1.6.3"
-# release "${commit_sha}" "${git_tag#v}"
-testPullThroughCache "${git_tag#v}"
+release "${commit_sha}" "${git_tag#v}"
