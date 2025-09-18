945d4080366a13b30063831efb801f9e0bbc091c
Derek Frank <dfrankmn@protonmail.com>
2025-09-18 16:03:22 -0700

    feat: Allow users to set spec.IpPrefixCount to pre-warm ip prefixes (#8480)

--- pkg/providers/amifamily/resolver.go
+++ pkg/providers/amifamily/resolver.go
@@ -66,6 +66,7 @@ type Options struct {
 	Labels                   map[string]string `hash:"ignore"`
 	KubeDNSIP                net.IP
 	AssociatePublicIPAddress *bool
+	IPPrefixCount            *int32
 	NodeClassName            string
 }
 
--- pkg/providers/launchtemplate/launchtemplate.go
+++ pkg/providers/launchtemplate/launchtemplate.go
@@ -215,6 +215,7 @@ func (p *DefaultProvider) CreateAMIOptions(ctx context.Context, nodeClass *v1.EC
 		CABundle:                 p.CABundle,
 		KubeDNSIP:                p.KubeDNSIP,
 		AssociatePublicIPAddress: nodeClass.Spec.AssociatePublicIPAddress,
+		IPPrefixCount:            nodeClass.Spec.IPPrefixCount,
 		NodeClassName:            nodeClass.Name,
 	}, nil
 }
@@ -274,9 +275,11 @@ func generateNetworkInterfaces(options *amifamily.LaunchTemplate, clusterIPFamil
 				//nolint: gosec
 				NetworkCardIndex: lo.ToPtr(int32(i)),
 				// Some networking magic to ensure that one network card has higher priority than all the others (important if an instance needs a public IP w/o adding an EIP to every network card)
-				DeviceIndex:   lo.ToPtr(lo.Ternary[int32](i == 0, 0, 1)),
-				InterfaceType: lo.ToPtr(string(ec2types.NetworkInterfaceTypeEfa)),
-				Groups:        lo.Map(options.SecurityGroups, func(s v1.SecurityGroup, _ int) string { return s.ID }),
+				DeviceIndex:     lo.ToPtr(lo.Ternary[int32](i == 0, 0, 1)),
+				InterfaceType:   lo.ToPtr(string(ec2types.NetworkInterfaceTypeEfa)),
+				Ipv4PrefixCount: lo.Ternary(clusterIPFamily == corev1.IPv6Protocol, nil, options.IPPrefixCount),
+				Ipv6PrefixCount: lo.Ternary(clusterIPFamily == corev1.IPv6Protocol, options.IPPrefixCount, nil),
+				Groups:          lo.Map(options.SecurityGroups, func(s v1.SecurityGroup, _ int) string { return s.ID }),
 				// Instances launched with multiple pre-configured network interfaces cannot set AssociatePublicIPAddress to true. This is an EC2 limitation. However, this does not apply for instances
 				// with a single EFA network interface, and we should support those use cases. Launch failures with multiple enis should be considered user misconfiguration.
 				AssociatePublicIpAddress: options.AssociatePublicIPAddress,
@@ -290,6 +293,8 @@ func generateNetworkInterfaces(options *amifamily.LaunchTemplate, clusterIPFamil
 		{
 			AssociatePublicIpAddress: options.AssociatePublicIPAddress,
 			DeviceIndex:              aws.Int32(0),
+			Ipv4PrefixCount:          lo.Ternary(clusterIPFamily == corev1.IPv6Protocol, nil, options.IPPrefixCount),
+			Ipv6PrefixCount:          lo.Ternary(clusterIPFamily == corev1.IPv6Protocol, options.IPPrefixCount, nil),
 			Groups: lo.Map(options.SecurityGroups, func(s v1.SecurityGroup, _ int) string {
 				return s.ID
 			}),
