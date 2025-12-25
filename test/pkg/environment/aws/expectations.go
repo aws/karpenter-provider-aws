ecfa33767c1e195f13a9c6668eaf3a2e96aa08d0
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2025-12-26 03:19:56 +0900

    chore: Refactor goroutine usage in ExpectMessagesCreated
    Replaces manual goroutine management with wg.Go for improved readability and safety. This change simplifies concurrent message sending in the ExpectMessagesCreated method.

--- test/pkg/environment/aws/expectations.go
+++ test/pkg/environment/aws/expectations.go
@@ -345,17 +345,15 @@ func (env *Environment) ExpectMessagesCreated(msgs ...interface{}) {
 
 	var err error
 	for _, msg := range msgs {
-		wg.Add(1)
-		go func(m interface{}) {
-			defer wg.Done()
+		wg.Go(func() {
 			defer GinkgoRecover()
-			_, e := env.SQSProvider.SendMessage(env.Context, m)
+			_, e := env.SQSProvider.SendMessage(env.Context, msg)
 			if e != nil {
 				mu.Lock()
 				err = multierr.Append(err, e)
 				mu.Unlock()
 			}
-		}(msg)
+		})
 	}
 	wg.Wait()
 	Expect(err).To(Succeed())
