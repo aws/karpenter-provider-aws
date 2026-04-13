271bd2ba680c8b0f9b21d3cec3df6b7ce918ee34
Keita Watanabe <keitaw09@gmail.com>
2026-04-14 07:39:43 +0900

    fix: nil pointer dereference in CapacityReservationFromEC2 for Interruptible field (#9080)

--- pkg/apis/v1/ec2nodeclass_status.go
+++ pkg/apis/v1/ec2nodeclass_status.go
@@ -261,7 +261,7 @@ func CapacityReservationFromEC2(clk clock.Clock, cr *ec2types.CapacityReservatio
 		InstanceType:          *cr.InstanceType,
 		OwnerID:               *cr.OwnerId,
 		ReservationType:       reservationType,
-		Interruptible:         lo.Ternary(cr.Interruptible == nil, false, *cr.Interruptible),
+		Interruptible:         lo.FromPtrOr(cr.Interruptible, false),
 		State:                 state,
 	}, nil
 }
