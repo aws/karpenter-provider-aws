daeb780105c9df0a466a7746b7181b30605a9660
Mayank <maydighe@amazon.com>
2026-05-08 11:30:28 -0700

    feat: expose AMI refresh interval as configurable setting
    Adds AMI_REFRESH_INTERVAL (--ami-refresh-interval, default 1m) to
    control how often Karpenter refreshes AMI data from EC2. This allows
    operators running large fleets in shared accounts to reduce
    DescribeImages API call volume by accepting longer AMI staleness.
    
    Reconciler requeue intervals are unchanged so that status updates
    remain responsive when fresh data becomes available.

--- pkg/test/options.go
+++ pkg/test/options.go
@@ -16,6 +16,7 @@ package test
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/imdario/mergo"
 	"github.com/samber/lo"
@@ -33,6 +34,7 @@ type OptionsFields struct {
 	InterruptionQueue       *string
 	ReservedENIs            *int
 	DisableDryRun           *bool
+	AMIRefreshInterval      *time.Duration
 }
 
 func Options(overrides ...OptionsFields) *options.Options {
@@ -52,5 +54,6 @@ func Options(overrides ...OptionsFields) *options.Options {
 		InterruptionQueue:       lo.FromPtrOr(opts.InterruptionQueue, ""),
 		ReservedENIs:            lo.FromPtrOr(opts.ReservedENIs, 0),
 		DisableDryRun:           lo.FromPtrOr(opts.DisableDryRun, false),
+		AMIRefreshInterval:      lo.FromPtrOr(opts.AMIRefreshInterval, time.Minute),
 	}
 }
