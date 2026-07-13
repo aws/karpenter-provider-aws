f537b01049d95a8fdb3b6ecabbe4ff7418b059f0
StableRelease <StableRelease@users.noreply.github.com>
2026-07-13 17:22:36 +0000

    Stable Release updates Release v1.4.3.

---
+++ website/content/en/v1.4/getting-started/migrating-from-cas/scripts/step01-env.sh
@@ -0,0 +1,8 @@
+AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
+AWS_REGION="$(aws configure list | grep region | tr -s " " | cut -d" " -f3)"
+OIDC_ENDPOINT="$(aws eks describe-cluster --name "${CLUSTER_NAME}" \
+    --query "cluster.identity.oidc.issuer" --output text)"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' \
+    --output text)
+K8S_VERSION=1.28
+ALIAS_VERSION="$(aws ssm get-parameter --name "/aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2023/x86_64/standard/recommended/image_id" --query Parameter.Value | xargs aws ec2 describe-images --query 'Images[0].Name' --image-ids | sed -r 's/^.*(v[[:digit:]]+).*$/\1/')"
